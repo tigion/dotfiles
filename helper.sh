8a65b05a1f747edf2c80310add7f9d3c7604bbcc
Christoph Zirkelbach <christoph@tigion.de>
2023-01-08 22:22:10 +0100

    Cleanup and separate in functions

--- helper.sh
+++ helper.sh
@@ -1,59 +1,117 @@
 #!/bin/bash
 
-# ...
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+# set colors
+COLOR_GRAY="\033[0;38;5;243m"
+COLOR_BLUE="\033[0;34m"
+COLOR_GREEN="\033[0;32m"
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_NONE="\033[0m"
+
+# set output
+#printf "\r  "
+#printf "\r\033[2K  ..."
+title () {
+  printf "\n${COLOR_GRAY}[${COLOR_BLUE} == ${COLOR_GRAY}]${COLOR_NONE}${COLOR_BLUE} ${1}${COLOR_NONE}\n"
 }
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+subtitle () {
+  printf "\n${COLOR_GRAY}[${COLOR_BLUE} -- ${COLOR_GRAY}]${COLOR_NONE}${COLOR_BLUE} ${1}${COLOR_NONE}\n"
+}
+info () {
+  printf "${COLOR_GRAY}[${COLOR_BLUE} .. ${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
+}
+question () {
+  printf "\n${COLOR_GRAY}[${COLOR_BLUE} ?? ${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
 }
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "${COLOR_GRAY}[${COLOR_GREEN} OK ${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
+}
+warning () {
+  printf "${COLOR_GRAY}[${COLOR_YELLOW}WARN${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
 }
 skip () {
-  printf "\r\033[2K  [\033[0;33mSKIP\033[0m] $1\n"
+  printf "${COLOR_GRAY}[${COLOR_YELLOW}SKIP${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
 }
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
+  printf "${COLOR_GRAY}[${COLOR_RED}FAIL${COLOR_GRAY}]${COLOR_NONE} ${1}\n"
+  exit 1
 }
 
-# ...
+testMessages () {
+  title "This is a title"
+  subtitle "Subtitle"
+  info "A little information"
+  question "Are you ready?"
+  success "Installed Neovim"
+  warning "Folder is empty"
+  skip "Configuration exists"
+  fail "File not find"
+}
+
+optionState () {
+  yes="${COLOR_GREEN}Yes${COLOR_NONE}"
+  no="${COLOR_RED}No${COLOR_NONE}"
+  if [[ "$1" == "true" ]]; then
+    echo "$yes"
+  else
+    echo "$no"
+  fi
+}
+
 showOptions () {
-  no="\033[0;31mNo\033[0m"
-  yes="\033[00;32mYes\033[0m"
-  
-  info "--- Options ---"
-  
-  if [[ "$INSTALL_SOFTWARE" == "true" ]]; then status="$yes"; else status="$no"; fi
-  info "Install software: $status"
-
-  if [[ "$INSTALL_CONFIG" == "true" ]]; then status="$yes"; else status="$no"; fi
-  info "Install configurations: $status"
-  
+  subtitle "Options:"
+  info "Install software: $(optionState $INSTALL_SOFTWARE)"
+  info "Install configurations: $(optionState $INSTALL_CONFIG)"
   if [[ "$INSTALL_CONFIG" == "true" ]]; then
-    if [[ "$OVERWRITE_EXISTING_CONFIG" == "true" ]]; then status="$yes"; else status="$no"; fi
-    info "- Overwrite existing ones: $status"
-  
+    info "- Overwrite existing ones: $(optionState $OVERWRITE_EXISTING_CONFIG)"
     if [[ "$OVERWRITE_EXISTING_CONFIG" == "true" ]]; then
-      if [[ "$BACKUP_EXISTING_CONFIG" == "true" ]]; then status="$yes"; else status="$no"; fi
-      info "- Backup existing ones: $status"
+      info "- Backup existing ones: $(optionState $BACKUP_EXISTING_CONFIG)"
       info "- Backup timestamp: $BACKUP_TIMESTAMP"
-
       if [[ "$BACKUP_EXISTING_CONFIG" == "true" ]]; then
-        if [[ "$BACKUP_EXISTING_CONFIG_LINK" == "true" ]]; then status="$yes"; else status="$no"; fi
-        info "- Backup symbolic links: $status"
+        info "- Backup symbolic links: $(optionState $BACKUP_EXISTING_CONFIG_LINK)"
       fi
     else
-      if [[ "$SKIP_EXISTING_CONFIG" == "true" ]]; then status="$yes"; else status="$no"; fi
-      info "- Skip existing config: $status"
+      info "- Skip existing config: $(optionState $SKIP_EXISTING_CONFIG)"
     fi
   fi
 }
 
+readyToStart () {
+  question "Ready to start installation (y/n)?"
+  read -n 1 answer
+  printf "\r"
+  if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
+    info "Bye"
+    exit 0
+  fi
+  info "$answer"
+}
+
+installXcodeCommandLineTools () {
+  subtitle "Xcode Command Line Tools"
+  result=`xcode-select -p 1>/dev/null;echo $?`
+  if [[ "$result" == 0 ]]; then
+    success "Command Line Tools for Xcode are already installed"
+  else
+    xcode-select --install
+    success "Installed Command Line Tools for Xcode"
+  fi
+}
+
+installHomebrew () {
+  subtitle "Homebrew"
+  if command -v brew &> /dev/null; then
+    success "Homebrew is already installed"
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    success "Installed Homebrew"
+  fi
+  brew bundle --file "$DOTFILES_ROOT/homebrew/Brewfile"
+  success "Installed Homebrew bundle"
+}
+
 # ...
-getConfigType () {
+configType () {
   if [[ -h "$1" ]]; then
     if [[ -e  "$1" ]]; then
       echo "link"
@@ -69,11 +127,10 @@ getConfigType () {
   fi
 }
 
-# ...
 link () {
   declare src="$1" dst="$2"
-  declare srcType=$(getConfigType "$src")
-  declare dstType=$(getConfigType "$dst")
+  declare srcType=$(configType "$src")
+  declare dstType=$(configType "$dst")
 
   # handle non-existent source or empty target
   if [[ ! -e "$src" ]]; then
@@ -126,4 +183,99 @@ link () {
   fi
   ln -s "$src" "$dst"
   success "Linked $srcType '$src' to '$dst'"
+}
+
+# bin
+useBin () {
+  subtitle "Bin"
+  link "$DOTFILES_ROOT/bin" "$HOME/bin"
+}
+
+# git
+useGit () {
+  if command -v git &> /dev/null; then
+    subtitle "Git"
+    link "$DOTFILES_ROOT/git/.gitignore" "$HOME/.gitignore"
+  fi
+}
+
+# zsh
+useZsh () {
+  #if command -v zsh &> /dev/null; then
+  if [[ "${SHELL##*/}" == "zsh" ]]; then
+    subtitle "Zsh"
+    # Oh My Zsh
+    if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+      #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+      success "Installed Oh My Zsh"
+    else
+      success "Oh My Zsh is already installed"
+    fi
+    # Powerlevel10k
+    if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+      #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+      success "Installed Powerlevel10k"
+    else
+      success "Powerlevel10k is already installed"
+    fi
+
+    link "$DOTFILES_ROOT/zsh/custom" "$HOME/.config/zsh"
+    link "$DOTFILES_ROOT/zsh/.zshrc" "$HOME/.zshrc"
+  fi
+}
+
+# bash
+useBash () {
+  #if command -v bash &> /dev/null; then
+  if [[ "${SHELL##*/}" == "bash" ]]; then
+    subtitle "Bash"
+    skip "TODO: Not yet supported!"
+  fi
+}
+
+# fd
+useFd () {
+  if command -v fd &> /dev/null; then
+    subtitle "fd"
+    link "$DOTFILES_ROOT/fd" "$HOME/.config/fd"
+  fi
+}
+
+# vim
+useVim () {
+  if command -v vim &> /dev/null; then
+    subtitle "Vim"
+    if [[ ! -f "$HOME/.vim/colors/monokai.vim" ]]; then 
+      #mkdir -p "$HOME/.vim/colors"
+      #curl -o https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim -f "$HOME/.vim/colors/monokai.vim"
+      success "Installed Monokai"
+    else
+      success "Monokai is already installed"
+    fi
+    if [[ ! -d "$HOME/.vim/pack/vendor/start/VimCompletesMe" ]]; then 
+      #mkdir -p ~/.vim/pack
+      #git clone git://github.com/ajh17/VimCompletesMe.git "$HOME/.vim/pack/vendor/start/VimCompletesMe"
+      success "Installed VimCompletesMe"
+    else
+      success "VimCompletesMe is already installed"
+    fi
+    link "$DOTFILES_ROOT/vim/.vimrc" "$HOME/.vimrc"
+  fi
+}
+
+# nvim
+useNeovim () {
+  if command -v zsh &> /dev/null; then
+    subtitle "Neovim"
+    link "$DOTFILES_ROOT/nvim" "$HOME/.config/nvim"
+    #nvim --headless +PackerInstall +q
+  fi
+}
+
+# kitty
+useKitty () {
+  if command -v kitty &> /dev/null; then
+    subtitle "Kitty"
+    link "$DOTFILES_ROOT/kitty" "$HOME/.config/kitty"
+  fi
 }
--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# TODO
-# 
-
 # cd & check
 if ! cd "$(dirname "$0")"; then exit; fi
 
@@ -10,6 +7,7 @@ if ! cd "$(dirname "$0")"; then exit; fi
 DOTFILES_ROOT="$(pwd)"
 
 # options
+# TODO: use command line options
 INSTALL_SOFTWARE=false
 INSTALL_CONFIG=true
 OVERWRITE_EXISTING_CONFIG=true
@@ -27,125 +25,28 @@ set -e
 source "${DOTFILES_ROOT}/helper.sh"
 
 # start
+title "Start installation"
 showOptions
-echo ''
-user 'Ready to start installation (y/n)?'
-read -n 1 answer
-if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
-  info "Bye"
-  exit
-fi
-info "$answer\n"
+readyToStart
 
-# 1. install software
+# install software
 if [[ "$INSTALL_SOFTWARE" == "true" ]]; then
-  info "--- Install Software ---"
-
-  # Xcode Command Line Tools
-  xx=`xcode-select -p 1>/dev/null;echo $?`
-  if [[ "$xx" == 0 ]]; then
-    success "Command Line Tools for Xcode are already installed"
-  else
-    xcode-select --install
-    success "Installed Command Line Tools for Xcode"
-  fi
-
-  # Homebrew
-  if command -v brew &> /dev/null; then
-    success "Homebrew is already installed"
-  else
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    success "Installed Homebrew"
-  fi
-  brew bundle --file "$DOTFILES_ROOT/homebrew/Brewfile"
-  success "Installed Homebrew bundle"
-  echo ''
+  title "Install Software"
+  installXcodeCommandLineTools
+  installHomebrew
 fi
 
-# 2. link configs and install dependies
+# link configs and install dependies
 if [[ "$INSTALL_CONFIG" == "true" ]]; then
-  info "--- Install Configurations ---"
-
-  # bin (own scripts)
-  info "Bin:"
-  link "$DOTFILES_ROOT/bin" "$HOME/bin"
-
-  # git
-  if command -v git &> /dev/null; then
-    info "Git:"
-    link "$DOTFILES_ROOT/git/.gitignore" "$HOME/.gitignore"
-  fi
-  
-  # zsh
-  #if command -v zsh &> /dev/null; then
-  if [[ "${SHELL##*/}" == "zsh" ]]; then
-    info "Zsh:"
-    # Oh My Zsh
-    if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-      #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-      success "Installed Oh My Zsh"
-    else
-      success "Oh My Zsh is already installed"
-    fi
-    # Powerlevel10k
-    if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-      #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-      success "Installed Powerlevel10k"
-    else
-      success "Powerlevel10k is already installed"
-    fi
-
-    link "$DOTFILES_ROOT/zsh/custom" "$HOME/.config/zsh"
-    link "$DOTFILES_ROOT/zsh/.zshrc" "$HOME/.zshrc"
-  fi
-
-  # bash
-  #if command -v bash &> /dev/null; then
-  if [[ "${SHELL##*/}" == "bash" ]]; then
-    info "Bash:"
-    skip "TODO: Not yet supported!"
-  fi
-  
-  # fd
-  if command -v fd &> /dev/null; then
-    info "fd:"
-    link "$DOTFILES_ROOT/fd" "$HOME/.config/fd"
-  fi
-  
-  # vim
-  if command -v vim &> /dev/null; then
-    info "Vim:"
-    if [[ ! -f "$HOME/.vim/colors/monokai.vim" ]]; then 
-      #mkdir -p "$HOME/.vim/colors"
-      #curl -o https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim -f "$HOME/.vim/colors/monokai.vim"
-      success "Installed Monokai"
-    else
-      success "Monokai is already installed"
-    fi
-    if [[ ! -d "$HOME/.vim/pack/vendor/start/VimCompletesMe" ]]; then 
-      #mkdir -p ~/.vim/pack
-      #git clone git://github.com/ajh17/VimCompletesMe.git "$HOME/.vim/pack/vendor/start/VimCompletesMe"
-      success "Installed VimCompletesMe"
-    else
-      success "VimCompletesMe is already installed"
-    fi
-    link "$DOTFILES_ROOT/vim/.vimrc" "$HOME/.vimrc"
-  fi
-
-  # nvim
-  if command -v zsh &> /dev/null; then
-    info "Neovim:"
-    link "$DOTFILES_ROOT/nvim" "$HOME/.config/nvim"
-    #nvim --headless +PackerInstall +q
-  fi
-
-  # kitty
-  if command -v kitty &> /dev/null; then
-    info "Kitty:"
-    link "$DOTFILES_ROOT/kitty" "$HOME/.config/kitty"
-  fi
-
-  echo ''
+  title "Install Configurations"
+  useBin
+  useGit
+  useZsh
+  useBash
+  useFd
+  useVim
+  useNeovim
+  useKitty
 fi
 
 info "Finished"
