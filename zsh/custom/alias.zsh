5b2be6d5027ecb3ea707a41ab051bb4abda9a1e0
Christoph Zirkelbach <christoph@tigion.de>
2023-01-08 18:32:49 +0100

    Add alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,7 +1,13 @@
 # --- Aliases ---
 
+# open tree without .git folder and ignored files
 alias tree="tree -a -I '.git' --gitignore --dirsfirst"
 
+# open todo note in Bear.app
 alias todo="bear open-todo"
 
+# recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+
+# ...
 alias ti-clock="printf '\e[8;11;48t'; tty-clock -c -C3 -f '%A %d.%m.%Y'"
