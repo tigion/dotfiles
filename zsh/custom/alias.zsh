036a293b136d3f3f70f2c762d6e4b17ace1d3d24
Christoph Zirkelbach <christoph@tigion.de>
2023-01-10 21:32:07 +0100

    Update aliases

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -3,11 +3,22 @@
 # open tree without .git folder and ignored files, follow symbolic links
 alias tree="tree -a -l -I '.git' --gitignore --dirsfirst"
 
-# open todo note in Bear.app
-alias todo="bear open-todo"
+# echo PATH variable line by line
+alias echoPATH="echo '${PATH//:/\\n}'"
+
+# open tty-clock with my preferred settings
+alias ti-clock="tty-clock -c -C3 -f '%A %d.%m.%Y'"
+
+# -- macOS --
 
 # recursively delete `.DS_Store` files
 alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
-# ...
-alias ti-clock="printf '\e[8;11;48t'; tty-clock -c -C3 -f '%A %d.%m.%Y'"
+# quickly lock or put the Mac to sleep
+# System Settings -> Lock Screen -> Require password to wake this computer from sleep or screensaver -> immediately 
+alias afk="open -a ScreenSaverEngine.app"
+#alias afk="pmset displaysleepnow"
+alias slp="pmset sleepnow"
+
+# open todo note in Bear.app
+alias todo="bear open-todo"
