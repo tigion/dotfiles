c268a4968d2a96f67bca9bf496340b5f3b54cf44
Christoph Zirkelbach <christoph@tigion.de>
2023-02-05 17:22:57 +0100

    Update footer (version, plugin count, date)

--- nvim/after/plugin/alpha-nvim.lua
+++ nvim/after/plugin/alpha-nvim.lua
@@ -52,10 +52,11 @@ dashboard.section.buttons.val = {
 -- Footer
 local function footer()
   local version = vim.version()
+  local pluginCount = vim.fn.len(vim.fn.globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))
   local print_version = 'v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
+  local date = os.date '%d.%m.%Y'
   --local datetime = os.date '%d.%m.%Y %H:%M'
-  --return print_version .. ' - ' .. datetime
-  return print_version
+  return ' ' .. print_version .. '   ' .. pluginCount .. '   ' .. date
 end
 
 dashboard.section.footer.val = footer()
