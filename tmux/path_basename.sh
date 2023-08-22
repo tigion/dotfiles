30b867239707631f7ca86ee1a2e5228ea17f14ad
Christoph Zirkelbach <christoph@tigion.de>
2023-08-22 12:26:02 +0200

    Add symlink handling

--- tmux/path_basename.sh
+++ tmux/path_basename.sh
@@ -12,10 +12,20 @@
 # Replace `#{b:pane_current_path}` in tmux.conf with
 # `#(~/.config/tmux/path_basename.sh #{pane_current_path})`
 
-[[ -n $1 ]] && path=$1 || path="-"
+path=${1:-"-"}
+# [[ -n $1 ]] && path=$1 || path="-"
 
 if [[ ${path::1} == "/" ]]; then
-  path=~${path#"$HOME"}
+  # use only real paths
+  path=~${path#"$(realpath "$HOME")"}
+
+  # use symlinks and real paths
+  # home=$(realpath "$HOME"
+  # if [[ ${path} == "$HOME"* ]]; then
+  #   path=~${path#"$HOME"}
+  # elif [[ "$HOME" != "$home" ]]; then
+  #   path=~${path#"$home"}
+  # fi
 fi
 
 basename "${path}"
