de2e1aebd84b045df749da69b1ff5f2f90c407f0
Christoph Zirkelbach <christoph@tigion.de>
2023-08-29 15:10:12 +0200

    Switch from packer.nvim to lazy.nvim (close #1)
    - [optional] remove local nvim data for fresh start with lazy:
      `rm -rf  ~/.local/share/nvim ~/.local/state/nvim`
    - if needed, copy nvim-packer to .config and start old nvim packer config with:
      `NVIM_APPNAME=nvim-packer nvim`

--- nvim-lazy/lua/tigion/init.lua
+++
@@ -1,7 +0,0 @@
---print("Hello from lua/tigion/init.lua")
-
--- core
-require('tigion.core')
-
--- plugins
-require('tigion.lazy')
--- nvim/lua/tigion/init.lua
+++ nvim/lua/tigion/init.lua
@@ -1,16 +1,7 @@
 --print("Hello from lua/tigion/init.lua")
 
-require 'tigion.base'
-require 'tigion.highlights'
-require 'tigion.keymaps'
-require 'tigion.plugins'
+-- core
+require('tigion.core')
 
-local has = vim.fn.has
-local isMac = has 'macunix'
-local isWin = has 'win32'
-
-if isMac then
-  require 'tigion.macos'
-elseif isWin then
-  require 'tigion.windows'
-end
+-- plugins
+require('tigion.lazy')
