9a86925baf701e53b15507be30d971f619888c49
Christoph Zirkelbach <christoph@tigion.de>
2023-09-04 22:52:57 +0200

    Update ...

--- asciidoctor/install_asciidoctor.sh
+++ asciidoctor/install_asciidoctor.sh
@@ -10,39 +10,39 @@ set -e
 
 # helper
 
-# check OS
-is_linux() {
-  [[ $(uname -s) == "Linux" ]]
-}
-is_macos() {
-  [[ $(uname -s) == "Darwin" ]]
-}
+# check OS and platform
+is_linux() { [[ $(uname -s) == "Linux" ]]; }
+is_macos() { [[ $(uname -s) == "Darwin" ]]; }
+is_arm64() { [[ "$(uname -m)" == "arm64" ]]; }
+is_x86_64() { [[ "$(uname -m)" == "x86_64" ]]; }
 
 # check if a command exists
-is_command() {
-  command -v "$1" &>/dev/null
-}
+is_command() { command -v "$1" &>/dev/null; }
 
 # OS dependent installation
 install() {
-  echo "> Installing '$1' ..."
-  if is_macos; then
-    brew install "$1"
-  elif is_linux; then
-    sudo apt install "$1"
-  fi
+	echo "> Installing '$1' ..."
+	if is_macos; then
+		brew install "$1"
+	elif is_linux; then
+		sudo apt install "$1"
+	fi
 }
 
 # install Asciidoctor
 
-# requirements
+# ruby
 install ruby
+
 # java (for asciidoctor-diagram)
 install openjdk
-# macOS:
 if is_macos; then
-  sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-  # echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+	# brew info openjdk | sed '/==> Caveats/,/==>/!d;//d'
+	is_arm64 && src_path="/opt/homebrew" || src_path="/usr/local"
+  if [[ -f "${src_path}/opt/openjdk/libexec/openjdk.jdk" ]]; then
+	  sudo ln -sfn "${src_path}/opt/openjdk/libexec/openjdk.jdk" "/Library/Java/JavaVirtualMachines/openjdk.jdk"
+  fi
+	#echo "export PATH=\"${src_path}/opt/openjdk/bin:$PATH\"" >> ~/.zshrc
 fi
 
 # asciidoctor
@@ -54,9 +54,3 @@ gem install text-hyphen
 # diagram (plantuml) support
 install graphviz
 gem install asciidoctor-diagram
-
-if is_macos; then
-  printf "\nThings manual to do (if needed):\n"
-  printf "openjdk:\n"
-  brew info openjdk | sed '/==> Caveats/,/==>/!d;//d'
-fi
