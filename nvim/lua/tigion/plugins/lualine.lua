51cb227d6ec8fae79860104364ceabac4008f530
Christoph Zirkelbach <christoph@tigion.de>
2023-09-11 08:07:20 +0200

    Update config

--- nvim/lua/tigion/plugins/lualine.lua
+++ nvim/lua/tigion/plugins/lualine.lua
@@ -18,9 +18,9 @@ return {
         lualine_b = { 'branch' },
         lualine_c = { {
           'filename',
-          file_status = true, -- displays file status (readonly status, modified status)
-          path = 0,           -- 0 = just filename, 1 = relative path, 2 = absolute path
-        }, },
+          file_status = true,
+          path = 0,
+        } },
         lualine_x = {
           {
             'diagnostics',
@@ -28,6 +28,7 @@ return {
             symbols = icons.diagnostics,
           },
           'encoding',
+          'fileformat',
           'filetype',
         },
         lualine_y = { 'progress' },
@@ -38,15 +39,15 @@ return {
         lualine_b = {},
         lualine_c = { {
           'filename',
-          file_status = true, -- displays file status (readonly status, modified status)
-          path = 1,           -- 0 = just filename, 1 = relative path, 2 = absolute path
-        }, },
+          file_status = true,
+          path = 1,
+        } },
         lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {},
       },
       tabline = {},
-      extensions = { 'fugitive' },
+      extensions = { 'nvim-tree' },
     }
   end,
 }
