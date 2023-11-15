dac4f728ce568ceef8f837cb7c205a9998014e96
Christoph Zirkelbach <christoph@tigion.de>
2023-11-15 15:26:32 +0100

    Update inc-rename config

--- nvim/lua/tigion/plugins/inc-rename.lua
+++ nvim/lua/tigion/plugins/inc-rename.lua
@@ -1,13 +1,27 @@
 return {
   'smjonas/inc-rename.nvim',
+  event = 'VeryLazy',
   cmd = 'IncRename',
-  config = true,
-  -- opts = {
-  --   input_buffer_type = 'dressing',
+  opts = {
+    -- input_buffer_type = 'dressing',
+  },
+  -- keys = {
+  --   {
+  --     '<leader>rn',
+  --     function()
+  --       return ':IncRename ' .. vim.fn.expand('<cword>')
+  --     end,
+  --     mode = { 'n' },
+  --     desc = '[r]e[n]ame',
+  --     expr = true,
+  --   },
   -- },
-  -- config = function()
-  --   require('inc_rename').setup({
-  --     input_buffer_type = 'dressing',
-  --   })
+  config = true,
+  -- config = function(_, opts)
+  --   require('inc_rename').setup(opts)
+  --
+  --   vim.keymap.set('n', '<leader>rm', function()
+  --     return ':IncRename ' .. vim.fn.expand('<cword>')
+  --   end, { expr = true })
   -- end,
 }
