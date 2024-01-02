94a41df60fc3adcae921625663b1bf0fcee615fe
Christoph Zirkelbach <christoph@tigion.de>
2024-01-02 12:33:17 +0100

    Update key bindings

--- nvim/lua/tigion/plugins/codeium.lua
+++ nvim/lua/tigion/plugins/codeium.lua
@@ -5,6 +5,7 @@ return {
     vim.keymap.set('n', 'coe', ':CodeiumEnable<CR>', { silent = true, desc = 'Enable Codeium' })
     vim.keymap.set('n', 'cod', ':CodeiumDisable<CR>', { silent = true, desc = 'Disable Codeium' })
     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<C-i>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
     vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
     vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
     vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
