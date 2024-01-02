4d55a68b567fd945700a11d531bbab6f35858297
Christoph Zirkelbach <christoph@tigion.de>
2024-01-02 12:40:26 +0100

    Add new toggle key binding

--- nvim/lua/tigion/plugins/codeium.lua
+++ nvim/lua/tigion/plugins/codeium.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     vim.keymap.set('n', 'coe', ':CodeiumEnable<CR>', { silent = true, desc = 'Enable Codeium' })
     vim.keymap.set('n', 'cod', ':CodeiumDisable<CR>', { silent = true, desc = 'Disable Codeium' })
+    vim.keymap.set('n', 'cot', ':CodeiumToggle<CR>', { silent = true, desc = 'Toggle Codeium' })
     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
     vim.keymap.set('i', '<C-i>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
     vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
