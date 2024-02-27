6207a3e31360e02106c62df07a1ffc7190fa07d5
Christoph Zirkelbach <christoph@tigion.de>
2024-02-27 20:35:30 +0100

    feat(neovim): add plugin illuminate

---
+++ nvim/lua/tigion/plugins/illuminate.lua
@@ -0,0 +1,27 @@
+return {
+  'RRethy/vim-illuminate',
+  event = 'VeryLazy',
+  opts = {
+    delay = 200,
+    large_file_cutoff = 2000,
+    large_file_overrides = {
+      providers = { 'lsp' },
+    },
+    filetypes_denylist = {
+      'dirbuf',
+      'dirvish',
+      'fugitive',
+      'nerdtree',
+      'NvimTree',
+    },
+    min_count_to_highlight = 2,
+  },
+  config = function(_, opts)
+    local illuminate = require('illuminate')
+
+    illuminate.configure(opts)
+
+    vim.keymap.set('n', '++', function() illuminate.goto_next_reference(false) end, { desc = 'Go to next reference' })
+    vim.keymap.set('n', 'üü', function() illuminate.goto_prev_reference(false) end, { desc = 'Go to prev reference' })
+  end,
+}
