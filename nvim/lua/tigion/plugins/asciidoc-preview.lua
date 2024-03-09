962e393bd50a6dafda80b947e7114cb7a0a13c19
Christoph Zirkelbach <christoph@tigion.de>
2024-03-09 18:44:39 +0100

    chore(neovim): update nvim-asciidoc-preview plugin settings

--- nvim/lua/tigion/plugins/asciidoc-preview.lua
+++ nvim/lua/tigion/plugins/asciidoc-preview.lua
@@ -1,9 +1,11 @@
 return {
-  'tigion/nvim-asciidoc-preview',
-  -- dir = '~/projects/neovim/nvim-asciidoc-preview',
+  'tigion/nvim-asciidoc-preview', -- variant for official plugin
+  -- dir = '~/projects/neovim/nvim-asciidoc-preview', -- variant for local plugin development
+
   -- enabled = false,
   cmd = { 'AsciiDocPreview' },
   ft = { 'asciidoc' },
+  build = 'cd server && npm install',
   opts = {
     server = {
       converter = 'js',
