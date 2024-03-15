f9859ad4ffb0fb49b148719f3888734c4032ecb7
Christoph Zirkelbach <christoph@tigion.de>
2024-03-15 10:42:26 +0100

    feat(neovim): use default options of asciidoc-preview plugin

--- nvim/lua/tigion/plugins/asciidoc-preview.lua
+++ nvim/lua/tigion/plugins/asciidoc-preview.lua
@@ -6,12 +6,5 @@ return {
   cmd = { 'AsciiDocPreview' },
   ft = { 'asciidoc' },
   build = 'cd server && npm install',
-  opts = {
-    server = {
-      converter = 'js',
-    },
-    preview = {
-      position = 'sync',
-    },
-  },
+  opts = {},
 }
