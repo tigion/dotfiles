d9718298792ecd9da30be8bab512bf2ed2e968ff
Christoph Zirkelbach <christoph@tigion.de>
2024-04-16 12:51:27 +0200

    feat(wezterm): update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,18 +10,20 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- Settings
+
 --config.color_scheme = 'Solarized Dark - Patched'
 --config.color_scheme = 'Catppuccin Mocha'
 --config.color_scheme = 'flexoki-dark'
-
 config.colors = { background = 'black' }
 config.window_background_opacity = 0.95
 config.font_size = 11
 --config.use_fancy_tab_bar = true
 
---- Set Pwsh (Powershell on Windows 10) as the default on Windows
-config.default_prog = { 'powershell.exe', '-NoLogo' }
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  -- Set Pwsh (Powershell on Windows 10) as the default on Windows
+  config.default_prog = { 'powershell.exe', '-NoLogo' }
+end
 
 -- and finally, return the configuration to wezterm
 return config
