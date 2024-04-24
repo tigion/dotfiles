4110cde8ff9b2ce22d3a7b0e9d8f5367f4c48d73
Christoph Zirkelbach <christoph@tigion.de>
2024-04-24 08:30:59 +0200

    feat(neovim): update config for commenting

--- nvim/lua/tigion/plugins/commenting.lua
+++ nvim/lua/tigion/plugins/commenting.lua
@@ -1,8 +1,25 @@
 return {
   { -- comment handling
     'numToStr/Comment.nvim',
-    opts = {},
+    -- opts = {},
     lazy = false,
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring', -- comment support for embedded languages
+    },
+    config = function()
+      -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
+      ---@diagnostic disable-next-line missing-fields
+      require('ts_context_commentstring').setup({
+        enable_autocmd = false,
+      })
+
+      ---@diagnostic disable-next-line missing-fields
+      require('Comment').setup({
+        -- hook for nvim-ts-context-commentstring
+        -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#commentnvim
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      })
+    end,
   },
 
   { -- highlight and search for todo comments
