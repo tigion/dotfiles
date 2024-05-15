77c0c1fca221085cd77f9bb37c28d88ee90711a3
Christoph Zirkelbach <christoph@tigion.de>
2024-05-15 10:58:02 +0200

    feat(tmux): add status bar command symbols

---
+++ tmux/cmd_symbol.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Returns a symbol for the given command name.
+# If no symbol is found, it returns the command name.
+# - $1: command name
+#
+# - `zsh` -> ``
+# - `foo` -> `foo`
+#
+# Replace `#{b:pane_current_command}` in tmux.conf with
+# `#(~/.config/tmux/cmd_symbol.sh #{b:pane_current_command})`
+
+case "$1" in
+  nvim)
+    echo ""
+    ;;
+  vim)
+    echo ""
+    ;;
+  bash | fish | zsh)
+    #     
+    # echo ""
+    echo ""
+    ;;
+  git | lazygit)
+    #  󰊢   
+    echo "󰊢"
+    ;;
+  ssh)
+    # echo ""
+    echo ""
+    ;;
+  *)
+    echo "$1"
+    ;;
+esac
