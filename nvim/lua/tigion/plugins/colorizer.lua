0e25c2c56b665911060a0c72ec99e678e25427fc
Christoph Zirkelbach <christoph@tigion.de>
2024-07-07 11:07:58 +0200

    chore(neovim): use NvChad version of nvim-colorizer.lua plugin

--- nvim/lua/tigion/plugins/colorizer.lua
+++ nvim/lua/tigion/plugins/colorizer.lua
@@ -1,10 +1,29 @@
+-- return {
+--   'norcalli/nvim-colorizer.lua', -- colored color codes
+--   enabled = true,
+--   opts = {
+--     '*',
+--     -- default: RGB, RRGGBB, names
+--     css = {
+--       css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+--     },
+--   },
+-- }
+
 return {
-  'norcalli/nvim-colorizer.lua', -- colored color codes
+  'NvChad/nvim-colorizer.lua', -- colored color codes
   enabled = true,
   opts = {
     -- default: RGB, RRGGBB, names
-    css = {
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    user_default_options = {
+      RGB = true,
+      RRGGBB = true,
+      names = false,
+      -- mode = 'virtualtext',
+    },
+    filetypes = {
+      '*', -- Highlight all files, but customize some others.
+      css = { css = true }, -- Enable parsing rgb(...) functions in css.
     },
   },
 }
