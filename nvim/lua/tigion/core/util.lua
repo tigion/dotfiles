4c07d4ddf9012bfc4da18101f79de63cf1049ccc
Christoph Zirkelbach <christoph@tigion.de>
2024-07-08 08:55:08 +0200

    refactor(neovim): use the new util modul in the mini.hipatterns config

--- nvim/lua/tigion/core/util.lua
+++ nvim/lua/tigion/core/util.lua
@@ -1,88 +1,34 @@
 local M = {}
 
--- Converts the RGB values to a HEX string.
---
----@param r integer The red value in the range 0-255.
----@param g integer The green value in the range 0-255.
----@param b integer The blue value in the range 0-255.
----@return string # The hexadecimal value in the format #rrggbb.
-function M.rgb_to_hex(r, g, b)
-  r = math.max(0, math.min(255, r))
-  g = math.max(0, math.min(255, g))
-  b = math.max(0, math.min(255, b))
-  return string.format('#%02x%02x%02x', r, g, b)
-end
+---Limits a value between min and max.
+---@param value number
+---@param min number
+---@param max number
+---@return number
+local function limit(value, min, max) return math.min(math.max(value, min), max) end
 
--- Converts the RGBA values to a HEX string.
---
----@param r integer The red value in the range 0-255.
----@param g integer The green value in the range 0-255.
----@param b integer The blue value in the range 0-255.
----@param a number The alpha value in the range 0-1.
----@return string # The hexadecimal value in the format #rrggbb.
-function M.rgba_to_hex(r, g, b, a)
-  r = math.max(0, math.min(255, r))
-  g = math.max(0, math.min(255, g))
-  b = math.max(0, math.min(255, b))
-  a = math.max(0, math.min(1, a))
-  return string.format('#%02x%02x%02x', r * a, g * a, b * a)
-end
+---Limits a value between 0 and 255.
+---@param value number
+---@return number
+local function limit_rgb(value) return limit(value, 0, 255) end
 
--- Converts the HSL values to a HEX string.
---
----@param h integer The hue value in degrees.
----@param s integer The saturation value in percent in the range 0-100.
----@param l integer The lightness value in percent in the range 0-100.
----@return string # The hexadecimal value in the format #rrggbb.
-function M.hsl_to_hex(h, s, l)
-  s = math.max(0, math.min(100, s))
-  l = math.max(0, math.min(100, l))
-  local r, g, b = M.hsl_to_rgb(h, s, l)
-  return string.format('#%02x%02x%02x', r, g, b)
-end
+---Limits a value between 0 and 100.
+---@param value number
+---@return number
+local function limit_percent(value) return limit(value, 0, 100) end
 
--- Converts the HSLA values to a HEX string.
---
----@param h integer The hue value in degrees.
----@param s integer The saturation value in percent in the range 0-100.
----@param l integer The lightness value in percent in the range 0-100.
----@param a number The alpha value in the range 0-1.
----@return string # The hexadecimal value in the format #rrggbb.
-function M.hsla_to_hex(h, s, l, a)
-  s = math.max(0, math.min(100, s))
-  l = math.max(0, math.min(100, l))
-  a = math.max(0, math.min(1, a))
-  local r, g, b = M.hsl_to_rgb(h, s, l)
-  return string.format('#%02x%02x%02x', r * a, g * a, b * a)
-end
+---Limits a value between 0 and 1.
+---@param value number
+---@return number
+local function limit_alpha(value) return limit(value, 0, 1) end
 
--- Converts CMYK values to a HEX string.
---
----@param c integer The cyan value in percent in the range 0-100.
----@param m integer The magenta value in percent in the range 0-100.
----@param y integer The yellow value in percent in the range 0-100.
----@param k integer The black value in percent in the range 0-100.
----@return string # The hexadecimal value in the format #rrggbb.
-function M.cmyk_to_hex(c, m, y, k)
-  c = math.max(0, math.min(100, c))
-  m = math.max(0, math.min(100, m))
-  y = math.max(0, math.min(100, y))
-  k = math.max(0, math.min(100, k))
-  c, m, y, k = c / 100, m / 100, y / 100, k / 100
-  local r = 255 * (1 - c) * (1 - k)
-  local g = 255 * (1 - m) * (1 - k)
-  local b = 255 * (1 - y) * (1 - k)
-  return string.format('#%02x%02x%02x', r, g, b)
-end
-
--- Converts HSL values to RGB values.
+---Converts HSL values to RGB values.
 -- https://www.w3.org/TR/css-color-3/#hsl-color
---
 ---@param h integer The hue value in degrees.
 ---@param s integer The saturation value in percent.
 ---@param l integer The lightness value in percent.
 ---@return integer, integer, integer
-function M.hsl_to_rgb(h, s, l)
+local function hsl_to_rgb(h, s, l)
   h, s, l = h % 360, s / 100, l / 100
   if h < 0 then h = h + 360 end
   local function f(n)
@@ -91,6 +37,69 @@ function M.hsl_to_rgb(h, s, l)
     return l - a * math.max(-1, math.min(k - 3, 9 - k, 1))
   end
   return f(0) * 255, f(8) * 255, f(4) * 255
+end
+
+M.color = {}
+
+---Converts the RGB values to a HEX string.
+---@param r integer The red value.
+---@param g integer The green value.
+---@param b integer The blue value.
+---@return string # The hexadecimal value in the format #rrggbb.
+function M.color.rgb_to_hex(r, g, b)
+  r, g, b = limit_rgb(r), limit_rgb(g), limit_rgb(b)
+  return string.format('#%02x%02x%02x', r, g, b)
+end
+
+---Converts the RGBA values to a HEX string.
+---@param r integer The red value.
+---@param g integer The green value.
+---@param b integer The blue value.
+---@param a number The alpha value in the range 0-1.
+---@return string # The hexadecimal value in the format #rrggbb.
+function M.color.rgba_to_hex(r, g, b, a)
+  r, g, b = limit_rgb(r), limit_rgb(g), limit_rgb(b)
+  a = limit_alpha(a)
+  return string.format('#%02x%02x%02x', r * a, g * a, b * a)
+end
+
+---Converts the HSL values to a HEX string.
+---@param h integer The hue value in degrees.
+---@param s integer The saturation value in percent.
+---@param l integer The lightness value in percent.
+---@return string # The hexadecimal value in the format #rrggbb.
+function M.color.hsl_to_hex(h, s, l)
+  s, l = limit_percent(s), limit_percent(l)
+  local r, g, b = hsl_to_rgb(h, s, l)
+  return string.format('#%02x%02x%02x', r, g, b)
+end
+
+---Converts the HSLA values to a HEX string.
+---@param h integer The hue value in degrees.
+---@param s integer The saturation value in percent.
+---@param l integer The lightness value in percent.
+---@param a number The alpha value in the range 0-1.
+---@return string # The hexadecimal value in the format #rrggbb.
+function M.color.hsla_to_hex(h, s, l, a)
+  s, l = limit_percent(s), limit_percent(l)
+  a = limit_alpha(a)
+  local r, g, b = hsl_to_rgb(h, s, l)
+  return string.format('#%02x%02x%02x', r * a, g * a, b * a)
+end
+
+---Converts CMYK values to a HEX string.
+---@param c integer The cyan value in percent.
+---@param m integer The magenta value in percent.
+---@param y integer The yellow value in percent.
+---@param k integer The black value in percent.
+---@return string # The hexadecimal value in the format #rrggbb.
+function M.color.cmyk_to_hex(c, m, y, k)
+  c, m, y, k = limit_percent(c), limit_percent(m), limit_percent(y), limit_percent(k)
+  c, m, y, k = c / 100, m / 100, y / 100, k / 100
+  local r = 255 * (1 - c) * (1 - k)
+  local g = 255 * (1 - m) * (1 - k)
+  local b = 255 * (1 - y) * (1 - k)
+  return string.format('#%02x%02x%02x', r, g, b)
 end
 
 return M
--- nvim/lua/tigion/plugins/mini-hipatterns.lua
+++ nvim/lua/tigion/plugins/mini-hipatterns.lua
@@ -8,107 +8,84 @@ return {
 
     local hipatterns = require('mini.hipatterns')
 
+    local rgb_to_hex = require('tigion.core.util').color.rgb_to_hex
+    local rgba_to_hex = require('tigion.core.util').color.rgba_to_hex
+    local hsl_to_hex = require('tigion.core.util').color.hsl_to_hex
+    local hsla_to_hex = require('tigion.core.util').color.hsla_to_hex
+    local cmyk_to_hex = require('tigion.core.util').color.cmyk_to_hex
+
+    -- Table with my styles and the corresponding group style in `MiniHipatterns.compute_hex_color_group`
+    local hi_group_styles = { fg = 'fg', bg = 'bg', line = 'line', inline = 'fg' }
+    -- The global style for my custom highlights
+    local my_style = 'inline' -- 'fg', 'bg', 'line' or 'inline'
+
     -- Returns hex color group for matching short hex color.
-    --
     ---@param match string
     ---@return string
     local hex_color_short = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
       local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
       local hex = string.format('#%s%s%s%s%s%s', r, r, g, g, b, b)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns hex color group for matching rgb() color.
-    --
     ---@param match string
     ---@return string
     local rgb_color = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
-      local red, green, blue = match:match('rgb%((%d+), ?(%d+), ?(%d+)%)')
-      local hex = string.format('#%02x%02x%02x', red, green, blue)
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+      local r, g, b = match:match('rgb%((%d+), ?(%d+), ?(%d+)%)')
+      local hex = rgb_to_hex(r, g, b)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
-    -- Returns hex color group for matching rgba() color
-    -- or false if alpha is nil or out of range.
+    -- Returns hex color group for matching rgba() color or false if alpha is nil.
     -- The use of the alpha value refers to a black background.
-    --
     ---@param match string
     ---@return string|false
     local rgba_color = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
-      local red, green, blue, alpha = match:match('rgba%((%d+), ?(%d+), ?(%d+), ?(%d*%.?%d*)%)')
-      alpha = tonumber(alpha)
-      if alpha == nil or alpha < 0 or alpha > 1 then return false end
-      local hex = string.format('#%02x%02x%02x', red * alpha, green * alpha, blue * alpha)
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+      local r, g, b, a = match:match('rgba%((%d+), ?(%d+), ?(%d+), ?(%d*%.?%d*)%)')
+      a = tonumber(a)
+      if a == nil then return false end
+      local hex = rgba_to_hex(r, g, b, a)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns hex color group for matching hsl() color.
-    --
     ---@param match string
-    ---@return string, integer, integer, integer
+    ---@return string --, integer, integer, integer
     local hsl_color = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
-      local hue, saturation, lightness = match:match('hsl%((%d+), ?(%d+)%%, ?(%d+)%%%)')
-
-      -- Converts HSL to RGB.
-      -- https://www.w3.org/TR/css-color-3/#hsl-color
-      --
-      ---@param h string The hue value in degrees.
-      ---@param s string The saturation value in percent.
-      ---@param l string The lightness value in percent.
-      ---@return integer, integer, integer
-      local function hsl_to_rgb(h, s, l)
-        h, s, l = h % 360, s / 100, l / 100
-        if h < 0 then h = h + 360 end
-        local function f(n)
-          local k = (n + h / 30) % 12
-          local a = s * math.min(l, 1 - l)
-          return l - a * math.max(-1, math.min(k - 3, 9 - k, 1))
-        end
-        return f(0) * 255, f(8) * 255, f(4) * 255
-      end
-
-      local red, green, blue = hsl_to_rgb(hue, saturation, lightness)
-      local hex = string.format('#%02x%02x%02x', red, green, blue)
-      return hipatterns.compute_hex_color_group(hex, style), red, green, blue
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+      local h, s, l = match:match('hsl%((%d+), ?(%d+)%%, ?(%d+)%%%)')
+      local hex = hsl_to_hex(h, s, l)
+      return hipatterns.compute_hex_color_group(hex, style) --, red, green, blue
     end
 
-    -- Returns hex color group for matching hsla() color
-    -- or false if alpha is nil or out of range.
+    -- Returns hex color group for matching hsla() color or false if alpha is nil.
     -- The use of the alpha value refers to a black background.
-    --
     ---@param match string
     ---@return string|false
     local hsla_color = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
-      local hue, saturation, lightness, alpha = match:match('hsla%((%d+), ?(%d+)%%, ?(%d+)%%, ?(%d*%.?%d*)%)')
-      alpha = tonumber(alpha)
-      if alpha == nil or alpha < 0 or alpha > 1 then return false end
-      local _, red, green, blue = hsl_color(_, string.format('hsl(%s, %s%%, %s%%)', hue, saturation, lightness))
-      local hex = string.format('#%02x%02x%02x', red * alpha, green * alpha, blue * alpha)
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+      local h, s, l, a = match:match('hsla%((%d+), ?(%d+)%%, ?(%d+)%%, ?(%d*%.?%d*)%)')
+      a = tonumber(a)
+      if a == nil then return false end
+      local hex = hsla_to_hex(h, s, l, a)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns hex color group for matching cmyk() color.
-    --
     ---@param match string
     ---@return string
     local cmyk_color = function(_, match)
-      local style = 'fg' -- 'fg' or 'bg', for extmark_opts_inline use 'fg'
-      local cyan, magenta, yellow, black = match:match('cmyk%((%d+)%%, ?(%d+)%%, ?(%d+)%%, ?(%d+)%%%)')
-      cyan, magenta, yellow, black = cyan / 100, magenta / 100, yellow / 100, black / 100
-      local red = 255 * (1 - cyan) * (1 - black)
-      local green = 255 * (1 - magenta) * (1 - black)
-      local blue = 255 * (1 - yellow) * (1 - black)
-      local hex = string.format('#%02x%02x%02x', red, green, blue)
+      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+      local c, m, y, k = match:match('cmyk%((%d+)%%, ?(%d+)%%, ?(%d+)%%, ?(%d+)%%%)')
+      local hex = cmyk_to_hex(c, m, y, k)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns extmark opts for highlights with virtual inline text.
-    --
     ---@param data table Includes `hl_group`, `full_match` and more.
     ---@return table
     local extmark_opts_inline = function(_, _, data)
@@ -120,6 +97,9 @@ return {
       }
     end
 
+    -- For my style `inline` use extra extmark opts.
+    local my_extmark_opts = my_style == 'inline' and extmark_opts_inline or nil
+
     hipatterns.setup({
       highlighters = {
         -- `#ff0000`
@@ -127,37 +107,37 @@ return {
         hex_color = hipatterns.gen_highlighter.hex_color({ style = 'inline', inline_text = ' ' }),
 
         -- `#f00`
-        hex_color_short = { pattern = '#%x%x%x%f[%X]', group = hex_color_short, extmark_opts = extmark_opts_inline },
+        hex_color_short = { pattern = '#%x%x%x%f[%X]', group = hex_color_short, extmark_opts = my_extmark_opts },
 
         -- `rgb(255, 0, 0)`
-        rgb_color = { pattern = 'rgb%(%d+, ?%d+, ?%d+%)', group = rgb_color, extmark_opts = extmark_opts_inline },
+        rgb_color = { pattern = 'rgb%(%d+, ?%d+, ?%d+%)', group = rgb_color, extmark_opts = my_extmark_opts },
 
         -- `rgba(255, 0, 0, 0.5)`
         rgba_color = {
           pattern = 'rgba%(%d+, ?%d+, ?%d+, ?%d*%.?%d*%)',
           group = rgba_color,
-          extmark_opts = extmark_opts_inline,
+          extmark_opts = my_extmark_opts,
         },
 
         -- `hsl(0, 100%, 50%)`
         hsl_color = {
           pattern = 'hsl%(%d+, ?%d+%%, ?%d+%%%)',
           group = hsl_color,
-          extmark_opts = extmark_opts_inline,
+          extmark_opts = my_extmark_opts,
         },
 
         -- `hsla(0, 100%, 50%, 0.5)`
         hsla_color = {
           pattern = 'hsla%(%d+, ?%d+%%, ?%d+%%, ?%d*%.?%d*%)',
           group = hsla_color,
-          extmark_opts = extmark_opts_inline,
+          extmark_opts = my_extmark_opts,
         },
 
         -- `cmyk(0%, 100%, 100%, 0%)`
         cmyk_color = {
           pattern = 'cmyk%(%d+%%, ?%d+%%, ?%d+%%, ?%d+%%%)',
           group = cmyk_color,
-          extmark_opts = extmark_opts_inline,
+          extmark_opts = my_extmark_opts,
         },
       },
     })
