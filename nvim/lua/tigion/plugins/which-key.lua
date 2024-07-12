44f914f0ca789853901f5f8566fc50d380c5822c
Christoph Zirkelbach <christoph@tigion.de>
2024-07-12 07:48:44 +0200

    refactor(neovim): rework which-key config

--- nvim/lua/tigion/plugins/which-key.lua
+++ nvim/lua/tigion/plugins/which-key.lua
@@ -11,12 +11,19 @@ return {
 
     -- TODO: keymap groups
     require('which-key').register({
-      ['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
-      ['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
-      -- ['<leader>l'] = { name = 'LSP', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = 'Toggle', _ = 'which_key_ignore' },
-      ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
-      ['ö'] = { name = 'Telescope', _ = 'which_key_ignore' },
+      -- ['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
+      -- ['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
+      -- -- ['<leader>l'] = { name = 'LSP', _ = 'which_key_ignore' },
+      -- ['<leader>t'] = { name = 'Toggle', _ = 'which_key_ignore' },
+      -- ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
+      -- ['ö'] = { name = 'Telescope', _ = 'which_key_ignore' },
+      ['<leader>'] = {
+        g = 'Git',
+        c = 'Code',
+        t = 'Toggle',
+        x = 'Trouble',
+      },
+      ['ö'] = 'Telescope',
     })
   end,
 }
