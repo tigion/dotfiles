6ebc23e4af7328fff4870413a038f53fe3d61f7a
Christoph Zirkelbach <christoph@tigion.de>
2024-08-13 15:35:36 +0200

    chore(neovim): update nvim-asciidoc-preview setup

--- nvim/lua/tigion/plugins/asciidoc-preview.lua
+++ nvim/lua/tigion/plugins/asciidoc-preview.lua
@@ -3,7 +3,6 @@ return {
 
   dev = false, -- When `true`, the local development version is used instead.
 
-  cmd = { 'AsciiDocPreview' },
   ft = { 'asciidoc' },
   build = 'cd server && npm install',
   opts = {
