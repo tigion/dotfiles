a1d4e750f8e8319f41d88f41456a9d698196c4a5
Christoph Zirkelbach <christoph@tigion.de>
2024-09-16 12:22:27 +0200

    feat(neovim): update colorscheme config and add color notes

---
+++ nvim/docs/colors.md
@@ -0,0 +1,47 @@
+# Colors
+
+## Tokyonight
+
+- Source: https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/colors
+
+| Name           | 'moon'  | 'storm' | 'night' | own     |
+| -------------- | ------- | ------- | ------- | ------- |
+| bg             | #222436 | #24283b | #1a1b26 |         |
+| bg_dark        | #1e2030 | #1f2335 | #16161e |         |
+| bg_highlight   | #2f334d | #292e42 | #292e42 |         |
+| blue           | #82aaff | #7aa2f7 | #7aa2f7 |         |
+| blue0          | #3e68d7 | #3d59a1 | #3d59a1 |         |
+| blue1          | #65bcff | #2ac3de | #2ac3de |         |
+| blue2          | #0db9d7 | #0db9d7 | #0db9d7 |         |
+| blue5          | #89ddff | #89ddff | #89ddff |         |
+| blue6          | #b4f9f8 | #b4f9f8 | #b4f9f8 |         |
+| blue7          | #394b70 | #394b70 | #394b70 |         |
+| comment        | #636da6 | #565f89 | #565f89 | #626784 |
+| cyan           | #86e1fc | #7dcfff | #7dcfff |         |
+| dark3          | #545c7e | #545c7e | #545c7e |         |
+| dark5          | #737aa2 | #737aa2 | #737aa2 |         |
+| fg             | #c8d3f5 | #c0caf5 | #c0caf5 |         |
+| fg_dark        | #828bb8 | #a9b1d6 | #a9b1d6 |         |
+| fg_gutter      | #3b4261 | #3b4261 | #3b4261 |         |
+| green          | #c3e88d | #9ece6a | #9ece6a |         |
+| green1         | #4fd6be | #73daca | #73daca |         |
+| green2         | #41a6b5 | #41a6b5 | #41a6b5 |         |
+| magenta        | #c099ff | #bb9af7 | #bb9af7 |         |
+| magenta2       | #ff007c | #ff007c | #ff007c |         |
+| orange         | #ff966c | #ff9e64 | #ff9e64 |         |
+| purple         | #fca7ea | #9d7cd8 | #9d7cd8 |         |
+| red            | #ff757f | #f7768e | #f7768e |         |
+| red1           | #c53b53 | #db4b4b | #db4b4b |         |
+| teal           | #4fd6be | #1abc9c | #1abc9c |         |
+| terminal_black | #444a73 | #414868 | #414868 |         |
+| yellow         | #ffc777 | #e0af68 | #e0af68 |         |
+| git add        | #b8db87 | #449dab | #449dab |         |
+| git change     | #7ca1f2 | #6183bb | #6183bb |         |
+| git delete     | #e26a75 | #914c54 | #914c54 |         |
+
+- 'night' is based on 'storm' but with darker `bg` and `bg_dark`
+
+### Telescope
+
+- darker bg for results: #14151f
+- more darker bg for preview:#0a0b10
--- nvim/lua/tigion/plugins/colorscheme.lua
+++ nvim/lua/tigion/plugins/colorscheme.lua
@@ -50,68 +50,34 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
-    style = 'moon', -- moon, storm, night, day
+    style = 'moon', -- moon, night, (storm, day)
     transparent = true,
     styles = {
-      -- Background styles. Can be "dark", "transparent" or "normal"
+      -- dark, transparent or normal
       sidebars = 'transparent',
       floats = 'dark',
     },
     lualine_bold = true,
+    -- Color code notes are in `nvim/docs/colors.md`
+    --
     -- TODO: Use `on_colors` or `style = `custom` to modify the colors?
     --       - https://github.com/folke/tokyonight.nvim/issues/595
+    --
     on_colors = function(colors)
-      -- https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/colors/moon.lua
-      -- bg             = "#222436",
-      -- bg_dark        = "#1e2030",
-      -- bg_highlight   = "#2f334d",
-      -- blue           = "#82aaff",
-      -- blue0          = "#3e68d7",
-      -- blue1          = "#65bcff",
-      -- blue2          = "#0db9d7",
-      -- blue5          = "#89ddff",
-      -- blue6          = "#b4f9f8",
-      -- blue7          = "#394b70",
-      -- comment        = "#636da6", -- '#727897'
-      -- cyan           = "#86e1fc",
-      -- dark3          = "#545c7e",
-      -- dark5          = "#737aa2",
-      -- fg             = "#c8d3f5",
-      -- fg_dark        = "#828bb8",
-      -- fg_gutter      = "#3b4261",
-      -- green          = "#c3e88d",
-      -- green1         = "#4fd6be",
-      -- green2         = "#41a6b5",
-      -- magenta        = "#c099ff",
-      -- magenta2       = "#ff007c",
-      -- orange         = "#ff966c",
-      -- purple         = "#fca7ea",
-      -- red            = "#ff757f",
-      -- red1           = "#c53b53",
-      -- teal           = "#4fd6be",
-      -- terminal_black = "#444a73",
-      -- yellow         = "#ffc777",
-      -- git = {
-      --   add          = "#b8db87",
-      --   change       = "#7ca1f2",
-      --   delete       = "#e26a75",
-      -- }
-      colors.comment = '#727897' -- use a more gray comment
+      colors.comment = '#626784' -- use a more gray comment
     end,
     on_highlights = function(hl, c)
-      -- lualine
-
       -- Telescope
-      local c1 = '#14151f'
-      hl.TelescopeBorder = { fg = c1, bg = c1 }
-      hl.TelescopeNormal = { fg = c.fg, bg = c1 }
+      local own_bg = '#14151f'
+      hl.TelescopeBorder = { fg = own_bg, bg = own_bg }
+      hl.TelescopeNormal = { fg = c.fg, bg = own_bg }
       hl.TelescopeTitle = { fg = c.c1, bg = c.blue }
       hl.TelescopePromptTitle = { fg = c.bg_float, bg = c.orange }
       hl.TelescopePromptBorder = { fg = c.bg_float, bg = c.bg_float }
       hl.TelescopePromptNormal = { fg = c.fg, bg = c.bg_float }
-      c1 = '#0a0b10'
-      hl.TelescopePreviewBorder = { fg = c1, bg = c1 }
-      hl.TelescopePreviewNormal = { fg = c.fg, bg = c1 }
+      own_bg = '#0a0b10'
+      hl.TelescopePreviewBorder = { fg = own_bg, bg = own_bg }
+      hl.TelescopePreviewNormal = { fg = c.fg, bg = own_bg }
     end,
   },
   config = function(_, opts)
