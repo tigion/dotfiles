7326c770fa70c5d9e375aec6628882c1c91f4411
Christoph Zirkelbach <christoph@tigion.de>
2024-09-29 17:01:00 +0200

    chore(tmux): update info script

--- scripts/tmux_info.sh
+++ scripts/tmux_info.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "tmux version:         $(tmux -V)"
+echo "platform:             $(uname -sp)"
+
 echo "pane_key_mode:        $(tmux display -p '#{pane_key_mode}')"
 echo "client_termtype:      $(tmux display -p '#{client_termtype}')"
 echo "extended-keys:        $(tmux display -p '#{extended-keys}')"
 echo "extended-keys-format: $(tmux display -p '#{extended-keys-format}')"
+
+echo "echo \$TERM:           ${TERM}"
+echo "tput colors:          $(tput colors)"
+echo "tput longname:        $(tput longname)"
