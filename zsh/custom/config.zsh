c9256489ab8c9aa891d64ea738b1baf2d79616a6
Christoph Zirkelbach <christoph@tigion.de>
2024-10-15 19:47:30 +0200

    feat(zsh): update zsh config

--- zsh/custom/config.zsh
+++ zsh/custom/config.zsh
@@ -9,6 +9,7 @@ setopt HIST_FIND_NO_DUPS # skip duplicate commands
 setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt HIST_IGNORE_ALL_DUPS   # delete all duplicates
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
