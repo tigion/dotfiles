d46c59ca01611e50ebca562f64dc5d9263586680
Christoph Zirkelbach <christoph@tigion.de>
2024-10-22 11:21:02 +0200

    feat(wezterm): update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,14 +12,30 @@ local config = wezterm.config_builder()
 
 -- Settings
 
---config.color_scheme = 'Solarized Dark - Patched'
---config.color_scheme = 'Catppuccin Mocha'
---config.color_scheme = 'flexoki-dark'
-config.colors = { background = 'black' }
+-- config.color_scheme = 'Solarized Dark - Patched'
+-- config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = 'flexoki-dark'
+config.color_scheme = 'Tokyo Night'
+-- config.colors = { background = 'black' }
 config.window_background_opacity = 0.95
-config.font_size = 11
+config.window_decorations = 'RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = false
 --config.use_fancy_tab_bar = true
 
+config.font = wezterm.font('MesloLGSDZ Nerd Font Mono')
+-- config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 17
+
+-- Platform specific settings
+
+-- wezterm.target_triple
+-- - x86_64-pc-windows-msvc - Windows
+-- - x86_64-apple-darwin - macOS (Intel)
+-- - aarch64-apple-darwin - macOS (Apple Silicon)
+-- - x86_64-unknown-linux-gnu - Linux
+-- wezterm.running_under_wsl
+-- - true/false
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- Set Pwsh (Powershell on Windows 10) as the default on Windows
   config.default_prog = { 'powershell.exe', '-NoLogo' }
