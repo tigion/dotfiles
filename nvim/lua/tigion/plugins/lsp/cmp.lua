71a6a83ec5a2f4cbe5c1924a6b950ea011426e09
Christoph Zirkelbach <christoph@tigion.de>
2025-01-01 13:36:53 +0100

    feat(nvim): add blink compat `omnifunc` completion source support

--- nvim/lua/tigion/plugins/lsp/cmp.lua
+++ nvim/lua/tigion/plugins/lsp/cmp.lua
@@ -1,13 +1,25 @@
 return {
   -- TODO:
   -- [ ] Update keymaps.md with cmp mappings and cleanup (remove unused nvim-cmp mappings)
-  -- [ ] Support for `omnifunc` (Ctrl-x Ctrl-o, `:h omnifunc`)
+  -- [x] Support for `omnifunc` (Ctrl-x Ctrl-o, `:h omnifunc`)
+  {
+    -- NOTE: Source provider for blink.cmp that allow you to use nvim-cmp completion sources.
+    --       Only for not supported native blink.cmp sources.
+    --
+    -- Needed for: `omnifunc` (hrsh7th/cmp-omni) completions
+    'saghen/blink.compat',
+    version = '*',
+    lazy = true,
+    opts = {},
+  },
   {
     'saghen/blink.cmp',
     enabled = true,
     lazy = false, -- lazy loading handled internally
-    -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      { 'rafamadriz/friendly-snippets' }, -- optional: provides snippets for the snippet source
+      { 'hrsh7th/cmp-omni' }, -- optional: provides `omnifunc` completions
+    },
 
     version = 'v0.*',
 
@@ -55,10 +67,14 @@ return {
         },
       },
 
+      fuzzy = {
+        use_typo_resistance = false,
+      },
+
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, via `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'omni' },
         -- optionally disable cmdline completions
         -- cmdline = {},
         providers = {
@@ -70,10 +86,19 @@ return {
             -- },
           },
           buffer = {
-            min_keyword_length = 3,
+            min_keyword_length = 2,
           },
           cmdline = {
             min_keyword_length = 2,
+          },
+          omni = {
+            name = 'omni', -- IMPORTANT: use the same name as you would for nvim-cmp
+            module = 'blink.compat.source',
+            score_offset = -3,
+            opts = {
+              disable_omnifuncs = { 'v:lua.vim.lsp.omnifunc' },
+            },
+            -- fallbacks = { 'buffer' },
           },
         },
       },
