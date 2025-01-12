a3f0c052e8336c29cd1a354f6465d3641eadb45b
Christoph Zirkelbach <christoph@tigion.de>
2025-01-12 21:05:22 +0100

    feat(zsh): update aliases

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,47 +1,72 @@
+# --- Helpers ---
+
+# Check if a command exists
+is_cmd() {
+  type "$1" >/dev/null 2>&1
+}
+
 # --- Aliases ---
 
-# open colored tree without .git folder and ignored files, follow symbolic links
-# alias tree='tree -a -l -C -I ".git" --gitignore --dirsfirst'
-alias tree='tree -a -l -C -I ".git" --dirsfirst'
+# source zsh config
+alias reload='
+  printf "cleanup aliases ... ";
+  unalias -a;
+  printf "done\nsource .zshrc ... ";
+  source ~/.zshrc; echo "done"'
 
 # echo PATH variable line by line
-alias echoPATH='echo "${PATH//:/\\n}"'
+alias path='echo "${PATH//:/\\n}"'
 
-# (fuzzy) search
-alias öh='history | cut -c 8- | fzf --scheme=history --tac --tmux --border-label="  History "'
-alias öa='alias | fzf --tmux --border-label="  Aliases "'
-# Searches for directories and files in the current directory and cd into it.
-# alias öd='tmp=$(find . -type d \( -path "*/.*" -o -path "./Library" -o -path "*/node_modules" \) -prune -o -type d -print | fzf) && cd "$tmp"'
-# alias öf='tmp=$(find . -type d \( -path "*/.*" -o -path "./Library" -o -path "*/node_modules" \) -prune -o -type f -print | fzf) && cd $(dirname "$tmp")'
-alias öd='tmp=$(fzf --tmux --border-label="  Directories " --walker dir,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd "$tmp"'
-alias öD='tmp=$(fzf --tmux --border-label="  Directories (~/) " --walker-root="$HOME" --walker dir,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd "$tmp"'
-alias öf='tmp=$(fzf --tmux --border-label="  Files " --walker file,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd $(dirname "$tmp")'
-alias öF='tmp=$(fzf --tmux --border-label="  Files (~/) " --walker-root="$HOME" --walker file,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd $(dirname "$tmp")'
+# open colored tree without .git folder, follow symbolic links
+alias tree='tree -a -l -C -I ".git" --dirsfirst'
 
-# replacements
-alias cat='bat -p'
-alias ls2='eza --icons'
-# alias tree='eza --tree -a -I .git --group-directories-first --icons'
-alias tree2='eza --tree -L 2 -a -I .git --group-directories-first --icons'
+# eza
+if is_cmd 'eza'; then
+  # defaults
+  alias eza='eza --group --group-directories-first --icons'
+  # shortcuts
+  alias ll='eza -l'
+  alias lt='eza --tree --all --ignore-glob=.git'
+  alias lt1='lt --level=1'
+  alias lt2='lt --level=2'
+  alias lt3='lt --level=3'
+  # replacements
+  alias ls='eza'
+fi
 
-# alias l="eza --icons"
-# alias ls="eza --icons"
-# alias ll="eza -lg --icons"
-# alias la="eza -lag --icons"
-# alias lt="eza -lTg --icons"
-# alias lt1="eza -lTg --level=1 --icons"
-# alias lt2="eza -lTg --level=2 --icons"
-# alias lt3="eza -lTg --level=3 --icons"
-# alias lta="eza -lTag --icons"
-# alias lta1="eza -lTag --level=1 --icons"
-# alias lta2="eza -lTag --level=2 --icons"
-# alias lta3="eza -lTag --level=3 --icons"
+# bat
+if is_cmd 'bat'; then
+  # replacements
+  alias cat='bat -pp'
+fi
 
-# shortcuts
-alias lg='lazygit'
-alias clock='tty-clock -c -C3 -f "%A %d.%m.%Y"'
+# lazygit
+if is_cmd 'lazygit'; then
+  # shortcuts
+  alias lg='lazygit'
+fi
 
-# Python
+# tty-clock
+if is_cmd 'tty-clock'; then
+  # shortcuts
+  alias clock='tty-clock -c -C3 -f "%A %d.%m.%Y"'
+fi
+
+# fzf (fuzzy) search
+if is_cmd 'fzf'; then
+  alias öh='history | cut -c 8- | fzf --scheme=history --tac --tmux --border-label="  History "'
+  alias öa='alias | fzf --tmux --border-label="  Aliases "'
+  # Searches for directories and files in the current directory and cd into it.
+  alias öd='tmp=$(fzf --tmux --border-label="  Directories " --walker dir,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd "$tmp"'
+  # alias öd='tmp=$(find . -type d \( -path "*/.*" -o -path "./Library" -o -path "*/node_modules" \) -prune -o -type d -print | fzf) && cd "$tmp"'
+  alias öD='tmp=$(fzf --tmux --border-label="  Directories (~/) " --walker-root="$HOME" --walker dir,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd "$tmp"'
+  alias öf='tmp=$(fzf --tmux --border-label="  Files " --walker file,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd $(dirname "$tmp")'
+  # alias öf='tmp=$(find . -type d \( -path "*/.*" -o -path "./Library" -o -path "*/node_modules" \) -prune -o -type f -print | fzf) && cd $(dirname "$tmp")'
+  alias öF='tmp=$(fzf --tmux --border-label="  Files (~/) " --walker-root="$HOME" --walker file,follow,hidden --walker-skip .git,node_modules,target,.venv,Library,Applications) && cd $(dirname "$tmp")'
+fi
+
+# -- Python --
+
 # virtual environment
 alias vc='python3 -m venv ./venv'
 alias va='source ./venv/bin/activate'
@@ -58,6 +83,3 @@ alias cleanup='find . -name "*.DS_Store" -type f -ls -delete'
 alias afk='open -a ScreenSaverEngine.app'
 #alias afk="pmset displaysleepnow"
 alias slp='pmset sleepnow'
-
-# open todo note in Bear.app
-alias todo='bear open-todo'
