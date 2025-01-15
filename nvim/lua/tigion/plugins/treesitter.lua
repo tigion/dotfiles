6c422f396d62674373fa3d41a0e8ed3d5501ada6
Christoph Zirkelbach <christoph@tigion.de>
2025-01-15 22:38:50 +0100

    feat(nvim): test mini.ai and treesitter text-objects (deactivated)

--- nvim/lua/tigion/plugins/treesitter.lua
+++ nvim/lua/tigion/plugins/treesitter.lua
@@ -1,69 +1,78 @@
 return {
-  -- This plugin adds a Treesitter configuration and abstraction layer to Neovim.
-  -- Link: https://github.com/nvim-treesitter/nvim-treesitter
+  {
+    -- This plugin adds a Treesitter configuration and abstraction layer to Neovim.
+    -- Link: https://github.com/nvim-treesitter/nvim-treesitter
 
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local status, ts = pcall(require, 'nvim-treesitter.configs')
-    if not status then return end
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local status, ts = pcall(require, 'nvim-treesitter.configs')
+      if not status then return end
 
-    -- NOTE: The following line suppresses the warning 'Missing required fields'
-    -- Source: https://github.com/LuaLS/lua-language-server/issues/2214
-    -- Alternative add an global `disable = { 'missing-fields' }` to the lua_la config above
-    --
-    ---@diagnostic disable-next-line missing-fields
-    ts.setup({
-      highlight = {
-        enable = true,
-        disable = {},
-      },
-      indent = {
-        enable = true,
-        disable = {},
-      },
-      ensure_installed = {
-        'bash',
-        'c',
-        'css',
-        'dockerfile',
-        'gitignore',
-        'html',
-        'htmldjango',
-        'java',
-        'javascript',
-        'json',
-        'lua',
-        'luadoc',
-        'make',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'phpdoc',
-        'python',
-        'toml',
-        'tsx',
-        'vim',
-        'vimdoc',
-        'vue',
-        'yaml',
-        --'help',
-        --'swift',
-      },
-      auto_install = true,
-      -- for incremental selection
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
-          scope_incremental = false,
-          node_decremental = '<BS>',
+      -- NOTE: The following line suppresses the warning 'Missing required fields'
+      -- Source: https://github.com/LuaLS/lua-language-server/issues/2214
+      -- Alternative add an global `disable = { 'missing-fields' }` to the lua_la config above
+      --
+      ---@diagnostic disable-next-line missing-fields
+      ts.setup({
+        highlight = {
+          enable = true,
+          disable = {},
         },
-      },
-    })
+        indent = {
+          enable = true,
+          disable = {},
+        },
+        ensure_installed = {
+          'bash',
+          'c',
+          'css',
+          'dockerfile',
+          'gitignore',
+          'html',
+          'htmldjango',
+          'java',
+          'javascript',
+          'json',
+          'lua',
+          'luadoc',
+          'make',
+          'markdown',
+          'markdown_inline',
+          'php',
+          'phpdoc',
+          'python',
+          'toml',
+          'tsx',
+          'vim',
+          'vimdoc',
+          'vue',
+          'yaml',
+          --'help',
+          --'swift',
+        },
+        auto_install = true,
+        -- for incremental selection
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<CR>',
+            node_incremental = '<CR>',
+            scope_incremental = false,
+            node_decremental = '<BS>',
+          },
+        },
+      })
 
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
-  end,
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
+    end,
+  },
+
+  -- {
+  --   -- This plugin adds syntax aware text-objects to Neovim.
+  --   -- Link: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  --
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  -- },
 }
