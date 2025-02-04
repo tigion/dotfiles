152f31d6d848588b255b8d5e01410e5b43c0bdd1
Christoph Zirkelbach <christoph@tigion.de>
2025-02-04 14:14:18 +0100

    refactor(nvim): update snacks.dashboard config

--- nvim/lua/tigion/plugins/snacks.lua
+++ nvim/lua/tigion/plugins/snacks.lua
@@ -13,31 +13,24 @@ return {
     -- Beautiful declarative dashboards
     dashboard = {
       enabled = true,
-      width = math.min(math.max(vim.fn.winwidth(0) - 10, 40), 60),
+      width = math.min(math.max(vim.o.columns - 10, 40), 60),
       preset = {
-        keys = {
-          { icon = '󰈔 ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-          { icon = '󱔗 ', key = 'r', desc = 'Recent Files', action = ':Telescope oldfiles' },
-          { icon = ' ', key = 'l', desc = 'Load Session', action = ':Session load' },
-          { icon = '󰱼 ', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
-          { icon = '󰺮 ', key = 'g', desc = 'Find Text', action = ':Telescope live_grep' },
-          { icon = '󱤇 ', key = 'h', desc = 'Find Help Tag', action = ':Telescope live_grep' },
-          {
-            icon = ' ',
-            key = 's',
-            desc = 'Settings',
-            action = '<Cmd>cd ' .. vim.fn.stdpath('config') .. '<CR><Cmd>NvimTreeOpen<CR>',
-          },
-          { icon = ' ', key = 'p', desc = 'Check Plugins', action = ':Lazy' },
-          { icon = ' ', key = 'c', desc = 'Check Health', action = ':checkhealth' },
-          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
-        },
-        -- header = {
-        --   [[  _  _             _        ]],
-        --   [[ | \| |___ _____ _(_)_ __   ]],
-        --   [[ | .` / -_) _ \ V / | '  \  ]],
-        --   [[ |_|\_\___\___/\_/|_|_|_|_| ]],
-        -- },
+        keys = function()
+          local enabled = vim.o.lines >= 30
+          local settings_action = '<Cmd>cd ' .. vim.fn.stdpath('config') .. '<CR><Cmd>NvimTreeOpen<CR>'
+          return {
+            { icon = '󰈔 ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = '󱔗 ', key = 'r', desc = 'Recent Files', action = ':Telescope oldfiles' },
+            { icon = ' ', key = 'l', desc = 'Load Session', action = ':Session load' },
+            { icon = '󰱼 ', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
+            { icon = '󰺮 ', key = 'g', desc = 'Find Text', action = ':Telescope live_grep' },
+            { icon = '󱤇 ', key = 'h', desc = 'Find Help Tag', action = ':Telescope live_grep', enabled = enabled },
+            { icon = ' ', key = 's', desc = 'Settings', action = settings_action, enabled = enabled },
+            { icon = ' ', key = 'p', desc = 'Check Plugins', action = ':Lazy', enabled = enabled },
+            { icon = ' ', key = 'c', desc = 'Check Health', action = ':checkhealth', enabled = enabled },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          }
+        end,
         header = table.concat({
           [[  _  _             _        ]],
           [[ | \| |___ _____ _(_)_ __   ]],
@@ -50,23 +43,20 @@ return {
         { section = 'keys', gap = 1, padding = 1 },
         ---Returns a custom footer text section.
         function()
-          local nvim_version = require('tigion.core.util').info.nvim_version
-          local plugin_stats = require('tigion.core.util').info.plugin_stats
-          local version, date = nvim_version(), os.date('%d.%m.%Y')
-          local count = plugin_stats().count
-          local startuptime = plugin_stats().startuptime
-          local updates = ''
-          if plugin_stats().updates > 0 then updates = '  ' .. plugin_stats().updates .. '' end
+          local version = require('tigion.core.util').info.nvim_version()
+          local plugin_stats = require('tigion.core.util').info.plugin_stats()
+          local date = os.date('%d.%m.%Y')
+          local updates = plugin_stats.updates > 0 and '  ' .. plugin_stats.updates .. '' or ''
           return {
             align = 'center',
             text = {
               { ' ', hl = 'footer' },
-              { '' .. version, hl = 'NonText' },
+              { version, hl = 'NonText' },
               { '    ', hl = 'footer' },
-              { '' .. count, hl = 'NonText' },
+              { tostring(plugin_stats.count), hl = 'NonText' },
               { updates, hl = 'special' },
               { '   󰛕 ', hl = 'footer' },
-              { startuptime .. ' ms', hl = 'NonText' },
+              { plugin_stats.startuptime .. ' ms', hl = 'NonText' },
               { '    ', hl = 'footer' },
               { date, hl = 'NonText' },
             },
@@ -126,20 +116,9 @@ return {
     { '<Leader>Z', function() Snacks.zen.zoom() end, desc = 'Toggle Zen Zoom Mode' },
   },
 
-  config = function(_, opts)
-    local snacks = require('snacks')
-
-    -- Reduces the size of the dashboard for small window heights.
-    local win_height = vim.fn.winheight(0)
-    local dashboard_height = 4 + 1 + 2 * #opts.dashboard.preset.keys + 1 + 1
-    if win_height < dashboard_height + 3 then
-      opts.dashboard.preset.header = 'Neovim'
-      table.remove(opts.dashboard.preset.keys, #opts.dashboard.preset.keys - 1)
-      table.remove(opts.dashboard.preset.keys, #opts.dashboard.preset.keys - 1)
-      table.remove(opts.dashboard.preset.keys, #opts.dashboard.preset.keys - 1)
-      table.remove(opts.dashboard.preset.keys, #opts.dashboard.preset.keys - 1)
-    end
-
-    snacks.setup(opts)
-  end,
+  -- config = function(_, opts)
+  --   local snacks = require('snacks')
+  --
+  --   snacks.setup(opts)
+  -- end,
 }
