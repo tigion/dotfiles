b72e13a69421a1bd279c2099d7b5b06a1bb4eeff
Christoph Zirkelbach <christoph@tigion.de>
2025-02-10 12:14:44 +0100

    feat(nvim): update nvim-sessions config

--- nvim/lua/tigion/plugins/sessions.lua
+++ nvim/lua/tigion/plugins/sessions.lua
@@ -4,10 +4,13 @@ return {
 
   'tigion/nvim-sessions',
   dev = false,
+  event = 'VeryLazy',
   cmd = 'Session',
   keys = {
     { '<Leader>ws', '<Cmd>Session save<CR>', desc = 'Save session (cwd)' },
     { '<Leader>wl', '<Cmd>Session load<CR>', desc = 'Load session (cwd)' },
   },
-  opts = {},
+  opts = {
+    auto_save = false,
+  },
 }
