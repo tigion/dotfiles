c9d39fede16e468d554909a5de4ca87704d61c86
Christoph Zirkelbach <christoph@tigion.de>
2025-02-14 11:38:23 +0100

    feat(nvim): update and refactor snacks config

--- nvim/lua/tigion/plugins/snacks.lua
+++ nvim/lua/tigion/plugins/snacks.lua
@@ -5,10 +5,13 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+
   ---@module 'snacks'
   ---@type snacks.Config
+  ---@diagnostic disable-next-line: missing-fields
   opts = {
-    -- bigfile = { enabled = true },
+    -- Deal with big files
+    bigfile = { enabled = true },
 
     -- Beautiful declarative dashboards
     -- NOTE:
@@ -96,7 +99,17 @@ return {
           }
         end,
       },
+      config = function(opts, _)
+        -- Change dashboard header depending on the height of the screen.
+        if vim.o.lines < 37 then opts.sections[2].gap = 0 end
+        if vim.o.lines < 28 then opts.preset.header = 'N E O V I M' end
+      end,
     },
+
+    -- A file explorer (picker in disguise)
+    -- explorer = {
+    --   enabled = true,
+    -- },
 
     -- Indent guides and scopes
     indent = {
@@ -109,142 +122,7 @@ return {
     input = { enabled = true },
 
     -- A modern fuzzy-finder. (Like Telescope)
-    picker = {
-      -- The default layout preset.
-      layout = function()
-        return vim.o.columns >= 120 and 'my_telescope'
-          or vim.o.lines >= 25 and 'my_telescope_vertical'
-          or 'my_telescope_vertical_no_preview'
-      end,
-      matcher = {
-        frecency = true,
-      },
-      ui_select = true, -- Better `vim.ui.select` (Snacks.picker.select)
-      sources = {
-        files = { hidden = true }, -- Shows hidden files.
-        grep = { hidden = true }, -- Shows hidden files.
-        explorer = { hidden = true }, -- Shows hidden files.
-        select = {
-          layout = {
-            preset = 'my_select',
-          },
-        },
-      },
-      layouts = {
-        my_telescope = {
-          -- A borderless layout with a vertical split with a preview on the right.
-          -- - Based on the default `telescope` layout.
-          -- - Replaces `rounded` with `solid` borders.
-          reverse = true,
-          layout = {
-            box = 'horizontal',
-            backdrop = false,
-            width = 0.8,
-            height = 0.9,
-            border = 'none',
-            {
-              box = 'vertical',
-              { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
-              { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
-            },
-            {
-              win = 'preview',
-              title = '{preview:Preview}',
-              width = 0.45,
-              border = 'solid',
-              title_pos = 'center',
-            },
-          },
-        },
-        my_telescope_vertical = {
-          -- A borderless layout with a vertical split with a preview on the top.
-          -- - Based on the `my_telescope` layout.
-          reverse = true,
-          layout = {
-            box = 'vertical',
-            backdrop = false,
-            width = 0.8,
-            height = 0.9,
-            border = 'none',
-            {
-              win = 'preview',
-              title = '{preview:Preview}',
-              height = 0.4,
-              border = 'solid',
-              title_pos = 'center',
-            },
-            { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
-            { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
-          },
-        },
-        my_telescope_vertical_no_preview = {
-          -- A borderless layout with a vertical split without a preview on.
-          -- - Based on the `my_telescope_vertical` layout.
-          reverse = true,
-          layout = {
-            box = 'vertical',
-            backdrop = false,
-            width = 0.8,
-            height = 0.9,
-            border = 'none',
-            { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
-            { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
-          },
-        },
-        my_select = {
-          -- A borderless layout for the select picker.
-          -- Initial height of the inner root box is item count + 2
-          -- https://github.com/folke/snacks.nvim/blob/70afc4225ac8ae3e6c8af88d205b03991a173af3/lua/snacks/picker/select.lua#L37
-          -- FIX: Workaround to simulate a input box with solid border:
-          --      - in root box: (input + box) + list)
-          --      +----------+
-          --      |          | <- root box border top
-          --      +--------++|
-          --      | >      ||| <- input box no top or bottom border
-          --      +--------+||
-          --      |         || <- box border bottom
-          --      +---------+|
-          --      | 1.      || <- list box no border
-          --      | 2.      ||
-          --      +---------++
-          layout = {
-            backdrop = false,
-            width = 0.5,
-            min_width = 80,
-            height = 0.4,
-            min_height = 1,
-            border = 'top',
-            title = '{title}',
-            box = 'vertical', -- root box
-            {
-              box = 'vertical',
-              border = 'bottom', -- inner box top border = 1 line
-              height = 1,
-              { win = 'input', title = '{title}', height = 1, border = 'hpad' }, -- input = 1 line
-            },
-            { win = 'list', border = 'none' }, -- list = #items lines
-          },
-        },
-      },
-      win = {
-        input = {
-          keys = {
-            ['<Esc>'] = { 'close', mode = { 'n', 'i' } }, -- Closes the picker on ESC instead of going to normal mode.
-          },
-        },
-        preview = {
-          wo = {
-            foldcolumn = '0',
-            number = false,
-            relativenumber = false,
-            signcolumn = 'no',
-          },
-        },
-      },
-      debug = {
-        scores = false,
-      },
-    },
+    -- picker: separate config -> snacks.picker.lua
 
     -- Pretty `vim.notify`
     notifier = { enabled = true },
@@ -283,65 +161,11 @@ return {
 
   -- Keymaps for the snacks plugin
   keys = {
+    -- explorer
+    -- { '<Leader>E', function() Snacks.explorer() end, desc = 'Toggle File Explorer' },
+
     -- notifier
     { '<Leader>tn', function() Snacks.notifier.show_history() end, desc = 'Toggle Notifier History' },
-
-    -- picker
-    -- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#-sources
-    -- :lua Snacks.picker.pickers()
-    -- Files
-    { 'Öf', function() Snacks.picker.files() end, desc = 'Find files (cwd)' },
-    {
-      'Ör',
-      function() Snacks.picker.recent({ filter = { cwd = true } }) end,
-      desc = 'Find recent files (cwd)',
-    },
-    -- Buffers
-    { 'Öb', function() Snacks.picker.buffers() end, desc = 'Find open buffers' },
-    -- Strings
-    { 'Ög', function() Snacks.picker.lines() end, desc = 'Find string (buffer)' },
-    { 'Ögg', function() Snacks.picker.grep() end, desc = 'Find string (cwd)' },
-    {
-      'Öw',
-      function() Snacks.picker.grep_word() end,
-      mode = { 'n', 'x' },
-      desc = 'Find current word/selection (cwd)',
-    },
-    -- Help tags
-    -- Diagnostics
-    { 'Öd', function() Snacks.picker.diagnostics_buffer() end, desc = 'Find diagnostics (buffer)' },
-    { 'Ödd', function() Snacks.picker.diagnostics() end, desc = 'Find diagnostics (cwd)' },
-    -- Commands and command history
-    { 'Ö:', function() Snacks.picker.commands() end, desc = 'Find commands' },
-    { 'Ö::', function() Snacks.picker.command_history() end, desc = 'Find command history' },
-    -- Search history
-    { 'Ö/', function() Snacks.picker.search_history() end, desc = 'Find search history' },
-    -- Git
-    { 'Öc', function() Snacks.picker.git_log_file() end, desc = 'Find git commits (file)' },
-    { 'Öcc', function() Snacks.picker.git_log() end, desc = 'Find git commits' },
-    -- Treesitter
-    {
-      'Ös',
-      -- function() Snacks.picker.treesitter({ tree = false }) end,
-      function() Snacks.picker.treesitter({ tree = false, filter = { default = true } }) end,
-      desc = 'Find Treesitter symbols',
-    },
-    {
-      'ÖS',
-      -- function() Snacks.picker.lsp_symbols({ tree = false }) end,
-      function() Snacks.picker.lsp_symbols({ tree = false, filter = { default = true, lua = true } }) end,
-      desc = 'Find LSP symbols',
-    },
-    {
-      'ÖSS',
-      function() Snacks.picker.lsp_workspace_symbols() end,
-      desc = 'Find LSP symbols (cwd)',
-    },
-    -- Register
-    { 'ÖR', function() Snacks.picker.registers() end, desc = 'Find registers' },
-    -- Snacks.picker
-    { 'ÖÖ', function() Snacks.picker.resume() end, desc = 'Reopen previous search' },
-    { 'ÖÖÖ', function() Snacks.picker.pickers() end, desc = 'Find picker sources' },
 
     -- scratch
     { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
@@ -360,12 +184,12 @@ return {
     local snacks = require('snacks')
 
     -- Change dashboard content depending on the height of the screen.
-    if vim.o.lines < 37 then opts.dashboard.sections[2].gap = 0 end
-    if vim.o.lines < 28 then opts.dashboard.preset.header = 'N E O V I M' end
+    -- if vim.o.lines < 37 then opts.dashboard.sections[2].gap = 0 end
+    -- if vim.o.lines < 28 then opts.dashboard.preset.header = 'N E O V I M' end
 
     snacks.setup(opts)
 
-    -- Dashboard autocommands
+    -- Workarounds
 
     -- FIX: This is a workaround to show updates in the dashboard at startup.
     --      The dashboard is loaded before lazy.nvim has the information
@@ -375,6 +199,9 @@ return {
       -- vim.notify('Dashboard updated', vim.log.levels.DEBUG, { title = 'Snacks Setup' })
     end, 1000)
 
+    -- Autocommands
+
+    -- Dashboard
     -- Updates an existing dashboard every time when lazy.nvim checks or updates plugins.
     vim.api.nvim_create_autocmd('User', {
       pattern = { 'LazyCheck', 'LazyUpdate' },
@@ -387,8 +214,7 @@ return {
       end,
     })
 
-    -- Notifier autocommands
-
+    -- Notifier
     -- LSP Progress - Advanced
     -- https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#-examples
     ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
---
+++ nvim/lua/tigion/plugins/snacks.picker.lua
@@ -0,0 +1,216 @@
+return {
+  -- This plugin adds a collection of small QoL plugins to Neovim.
+  -- Link: https://github.com/folke/snacks.nvim
+
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+
+  ---@module "snacks"
+  ---@type snacks.Config
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    -- A modern fuzzy-finder. (Like Telescope)
+    picker = {
+      -- The default layout preset.
+      layout = function()
+        return vim.o.columns >= 120 and 'my_telescope'
+          or vim.o.lines >= 25 and 'my_telescope_vertical'
+          or 'my_telescope_vertical_no_preview'
+      end,
+      matcher = {
+        frecency = true,
+      },
+      ui_select = true, -- Better `vim.ui.select` (Snacks.picker.select)
+      previewers = {
+        git = {
+          native = true, -- Use my local git previewer (delta).
+        },
+      },
+      sources = {
+        files = { hidden = true }, -- Shows hidden files.
+        grep = { hidden = true }, -- Shows hidden files.
+        explorer = {
+          hidden = true,
+          -- layout = { auto_hide = { 'input' } },
+        }, -- Shows hidden files.
+        select = { layout = { preset = 'my_select' } },
+      },
+      layouts = {
+        my_telescope = {
+          -- A borderless layout with a vertical split with a preview on the right.
+          -- - Based on the default `telescope` layout.
+          -- - Replaces `rounded` with `solid` borders.
+          reverse = true,
+          layout = {
+            box = 'horizontal',
+            backdrop = false,
+            width = 0.8,
+            height = 0.9,
+            border = 'none',
+            {
+              box = 'vertical',
+              { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
+              { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
+            },
+            {
+              win = 'preview',
+              title = '{preview:Preview}',
+              width = 0.45,
+              border = 'solid',
+              title_pos = 'center',
+            },
+          },
+        },
+        my_telescope_vertical = {
+          -- A borderless layout with a vertical split with a preview on the top.
+          -- - Based on the `my_telescope` layout.
+          reverse = true,
+          layout = {
+            box = 'vertical',
+            backdrop = false,
+            width = 0.8,
+            height = 0.9,
+            border = 'none',
+            {
+              win = 'preview',
+              title = '{preview:Preview}',
+              height = 0.4,
+              border = 'solid',
+              title_pos = 'center',
+            },
+            { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
+            { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
+          },
+        },
+        my_telescope_vertical_no_preview = {
+          -- A borderless layout with a vertical split without a preview on.
+          -- - Based on the `my_telescope_vertical` layout.
+          reverse = true,
+          layout = {
+            box = 'vertical',
+            backdrop = false,
+            width = 0.8,
+            height = 0.9,
+            border = 'none',
+            { win = 'list', title = ' Results ', title_pos = 'center', border = 'solid' },
+            { win = 'input', height = 1, border = 'solid', title = '{title} {live} {flags}', title_pos = 'center' },
+          },
+        },
+        my_select = {
+          -- A borderless layout for the select picker.
+          -- Initial height of the inner root box is item count + 2
+          -- https://github.com/folke/snacks.nvim/blob/70afc4225ac8ae3e6c8af88d205b03991a173af3/lua/snacks/picker/select.lua#L37
+          -- FIX: Workaround to simulate a input box with solid border:
+          --      - in root box: (input + box) + list)
+          --      +----------+
+          --      |          | <- root box border top
+          --      +--------++|
+          --      | >      ||| <- input box no top or bottom border
+          --      +--------+||
+          --      |         || <- box border bottom
+          --      +---------+|
+          --      | 1.      || <- list box no border
+          --      | 2.      ||
+          --      +---------++
+          layout = {
+            backdrop = false,
+            width = 0.5,
+            min_width = 80,
+            height = 0.4,
+            min_height = 1,
+            border = 'top',
+            title = '{title}',
+            box = 'vertical', -- root box
+            {
+              box = 'vertical',
+              border = 'bottom', -- inner box top border = 1 line
+              height = 1,
+              { win = 'input', title = '{title}', height = 1, border = 'hpad' }, -- input = 1 line
+            },
+            { win = 'list', border = 'none' }, -- list = #items lines
+          },
+        },
+      },
+      win = {
+        input = {
+          keys = {
+            ['<Esc>'] = { 'close', mode = { 'n', 'i' } }, -- Closes the picker on ESC instead of going to normal mode.
+          },
+        },
+        preview = {
+          wo = {
+            foldcolumn = '0',
+            number = false,
+            relativenumber = false,
+            signcolumn = 'no',
+          },
+        },
+      },
+      debug = {
+        scores = false,
+      },
+    },
+  },
+
+  -- Keymaps for the snacks plugin
+  keys = {
+    -- picker
+    -- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#-sources
+    -- :lua Snacks.picker.pickers()
+    -- Files
+    { 'Öf', function() Snacks.picker.files() end, desc = 'Find files (cwd)' },
+    {
+      'Ör',
+      function() Snacks.picker.recent({ filter = { cwd = true } }) end,
+      desc = 'Find recent files (cwd)',
+    },
+    -- Buffers
+    { 'Öb', function() Snacks.picker.buffers() end, desc = 'Find open buffers' },
+    -- Strings
+    { 'Ög', function() Snacks.picker.lines() end, desc = 'Find string (buffer)' },
+    { 'Ögg', function() Snacks.picker.grep() end, desc = 'Find string (cwd)' },
+    {
+      'Öw',
+      function() Snacks.picker.grep_word() end,
+      mode = { 'n', 'x' },
+      desc = 'Find current word/selection (cwd)',
+    },
+    -- Help tags
+    { 'Öh', function() Snacks.picker.help() end, desc = 'Find help tags' },
+    -- Diagnostics
+    { 'Öd', function() Snacks.picker.diagnostics_buffer() end, desc = 'Find diagnostics (buffer)' },
+    { 'Ödd', function() Snacks.picker.diagnostics() end, desc = 'Find diagnostics (cwd)' },
+    -- Commands and command history
+    { 'Ö:', function() Snacks.picker.commands() end, desc = 'Find commands' },
+    { 'Ö::', function() Snacks.picker.command_history() end, desc = 'Find command history' },
+    -- Search history
+    { 'Ö/', function() Snacks.picker.search_history() end, desc = 'Find search history' },
+    -- Git
+    { 'Öc', function() Snacks.picker.git_log_file() end, desc = 'Find git commits (file)' },
+    { 'Öcc', function() Snacks.picker.git_log() end, desc = 'Find git commits' },
+    -- Treesitter
+    {
+      'Ös',
+      -- function() Snacks.picker.treesitter({ tree = false }) end,
+      function() Snacks.picker.treesitter({ tree = false, filter = { default = true } }) end,
+      desc = 'Find Treesitter symbols',
+    },
+    {
+      'ÖS',
+      function() Snacks.picker.lsp_symbols({ tree = false }) end,
+      -- function() Snacks.picker.lsp_symbols({ tree = false, filter = { default = true, lua = true } }) end,
+      desc = 'Find LSP symbols',
+    },
+    {
+      'ÖSS',
+      function() Snacks.picker.lsp_workspace_symbols() end,
+      desc = 'Find LSP symbols (cwd)',
+    },
+    -- Register
+    { 'ÖR', function() Snacks.picker.registers() end, desc = 'Find registers' },
+    -- Snacks.picker
+    { 'ÖÖ', function() Snacks.picker.resume() end, desc = 'Reopen previous search' },
+    { 'ÖÖÖ', function() Snacks.picker.pickers() end, desc = 'Find picker sources' },
+  },
+}
