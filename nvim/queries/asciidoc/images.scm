a00da7cd35e540c1751a6b67c66ecfa94f5980e4
Christoph Zirkelbach <christoph@tigion.de>
2025-02-19 23:16:32 +0100

    feat(nvim): update queries/images.scm

--- nvim/queries/asciidoc/images.scm
+++ nvim/queries/asciidoc/images.scm
@@ -1,5 +1,10 @@
 
 (macro
   (name) @name (#eq? @name "image")
-  (target) @image.src
+  (target) @image.src (#match? @image.src "^[^{]")
+) @image
+
+(document_attribute
+  (attribute_name) @name (#eq? @name "imageFile")
+  (attribute_value) @image.src
 ) @image
