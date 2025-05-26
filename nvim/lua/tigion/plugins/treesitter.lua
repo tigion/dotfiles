0f2e61daaf24032437876424d4b705ebb36be8bd
Christoph Zirkelbach <christoph@tigion.de>
2025-05-26 08:56:31 +0200

    refactor(nvim): cleanup nvim-treesitter config and pin to master branch

--- nvim/lua/tigion/plugins/treesitter.lua
+++ nvim/lua/tigion/plugins/treesitter.lua
@@ -4,71 +4,73 @@ return {
     -- Link: https://github.com/nvim-treesitter/nvim-treesitter
 
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
-    config = function()
-      local status, ts = pcall(require, 'nvim-treesitter.configs')
-      if not status then return end
+    opts = {
+      -- Parsers to install
+      ensure_installed = {
+        'bash',
+        'c',
+        'css',
+        'dockerfile',
+        'gitignore',
+        'html',
+        'htmldjango',
+        'java',
+        'javascript',
+        'json',
+        'lua',
+        'luadoc',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'php',
+        'phpdoc',
+        'python',
+        'regex',
+        'toml',
+        'tsx',
+        'vim',
+        'vimdoc',
+        'vue',
+        'yaml',
+        --'help',
+        --'swift',
+      },
+      sync_install = false,
+      auto_install = true,
 
-      -- NOTE: The following line suppresses the warning 'Missing required fields'
-      -- Source: https://github.com/LuaLS/lua-language-server/issues/2214
-      -- Alternative add an global `disable = { 'missing-fields' }` to the lua_la config above
-      --
-      ---@diagnostic disable-next-line missing-fields
-      ts.setup({
-        highlight = {
-          enable = true,
-          disable = {},
+      -- Highlight
+      highlight = {
+        enable = true,
+        disable = {},
+      },
+
+      -- Incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>',
+          node_incremental = '<CR>',
+          scope_incremental = false,
+          node_decremental = '<BS>',
         },
-        indent = {
-          enable = true,
-          disable = {},
-        },
-        ensure_installed = {
-          'bash',
-          'c',
-          'css',
-          'dockerfile',
-          'gitignore',
-          'html',
-          'htmldjango',
-          'java',
-          'javascript',
-          'json',
-          'lua',
-          'luadoc',
-          'make',
-          'markdown',
-          'markdown_inline',
-          'php',
-          'phpdoc',
-          'python',
-          'regex',
-          'toml',
-          'tsx',
-          'vim',
-          'vimdoc',
-          'vue',
-          'yaml',
-          --'help',
-          --'swift',
-        },
-        auto_install = true,
-        -- for incremental selection
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<CR>',
-            node_incremental = '<CR>',
-            scope_incremental = false,
-            node_decremental = '<BS>',
-          },
-        },
-      })
+      },
+
+      -- Indention
+      indent = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
 
       local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+      -- tsx: Update filetypes
       parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
 
-      -- Adds a (experimental) parser for AsciiDoc.
+      -- asciidoc: Adds a (experimental) parser for AsciiDoc.
       -- Source: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#adding-parsers
       -- `:TSInstallFromGrammar asciidoc`
       -- local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
