80407529c020966354b982f626720b7d4de4d828
Christoph Zirkelbach <christoph@tigion.de>
2025-06-06 13:33:15 +0200

    feat(nvim): use master branch in telescope.nvim config

--- nvim/lua/tigion/plugins/telescope.lua
+++ nvim/lua/tigion/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   -- Link: https://github.com/nvim-telescope/telescope.nvim
 
   'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  -- branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, -- fuzzy find
