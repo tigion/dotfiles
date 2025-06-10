93cdcdbaa242d3d2db588f64304fa65143681f97
Christoph Zirkelbach <christoph@tigion.de>
2025-06-10 15:58:42 +0200

    feat(nvim): add filetype config for sql

---
+++ nvim/after/ftplugin/sql.lua
@@ -0,0 +1,4 @@
+-- Default to static completion for SQL
+-- let g:omni_sql_default_compl_type = 'syntax'
+-- https://github.com/neovim/neovim/issues/14433
+vim.g.omni_sql_default_compl_type = 'syntax'
