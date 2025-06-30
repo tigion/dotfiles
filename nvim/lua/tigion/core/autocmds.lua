268e2b37677c51416a6d18847e07c9237e83bbda
Christoph Zirkelbach <christoph@tigion.de>
2025-06-30 16:05:08 +0200

    fix(nvim): correct last location autocommand

--- nvim/lua/tigion/core/autocmds.lua
+++ nvim/lua/tigion/core/autocmds.lua
@@ -18,21 +18,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('BufReadPost', {
   desc = 'Go to last cursor location when opening a buffer',
   group = vim.api.nvim_create_augroup('go_to_last_location', { clear = true }),
-  callback = function(event)
-    local exclude = { 'gitcommit' } -- excluded filetypes
-    local buf = event.buf -- current buffer
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].got_last_location then return end
-    vim.b[buf].got_last_location = true
-    local mark = vim.api.nvim_buf_get_mark(buf, '"') -- get last cursor position
-    local line_count = vim.api.nvim_buf_line_count(buf)
+  callback = function(args)
+    -- FIX: The filetype is not yet set when the event `BufReadPost` is triggered.
+    --
+    -- local exclude = { gitcommit = true }
+    -- if exclude[vim.bo[args.buf].filetype] or vim.b[args.buf].got_last_location then return end
+
+    -- Don't fire twice for the same buffer.
+    if vim.b[args.buf].got_last_location then return end
+    vim.b[args.buf].got_last_location = true
+
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"') -- get last cursor position
+    -- Checks if the cursor in in the range of the file.
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
     if mark[1] > 0 and mark[1] <= line_count then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark) -- set and scroll to position in mark
-      -- middle line (if possible)
-      -- NOTE: This needs a short delay in order to work.
-      vim.defer_fn(function() vim.api.nvim_command([[keepjumps normal! zz]]) end, 10)
-      -- Other variants (optional with zz -> g`"zz):
-      -- vim.api.nvim_command([[keepjumps normal! g`"]])
-      -- vim.cmd('normal! g`"')
+      vim.api.nvim_buf_call(args.buf, function()
+        vim.cmd('keepjumps normal! g`"')
+        -- NOTE: `zz` needs a short delay in order to work in splits.
+        vim.defer_fn(function() vim.cmd('keepjumps normal! zz') end, 10)
+      end)
     end
   end,
 })
