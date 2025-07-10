44fe957b02378ba87bf8734d82868fad0efa9c03
Christoph Zirkelbach <christoph@tigion.de>
2025-07-10 08:28:52 +0200

    feat(nvim): update ts_ls lsp config

--- nvim/after/lsp/ts_ls.lua
+++ nvim/after/lsp/ts_ls.lua
@@ -6,40 +6,51 @@ local vue_typescript_plugin = vim.fn.expand('$MASON/bin/vue-language-server')
   .. '/node_modules/@vue/language-server'
   .. '/node_modules/@vue/typescript-plugin'
 
+-- local preferences = {
+--   importModuleSpecifierPreference = 'relative',
+--   includeCompletionsForModuleExports = true,
+--   includeCompletionsWithInsertText = true,
+--   quotePreference = 'auto',
+--   allowTextChangesInNewFiles = true,
+--   providePrefixAndSuffixTextForRename = true,
+--   allowRenameOfImportPath = true,
+--   provideRefactorNotApplicableReason = true,
+--   includeAutomaticOptionalChainCompletions = true,
+--   includeCompletionsForImportStatements = true,
+--   includeCompletionsWithSnippetText = true,
+--   completeFunctionCalls = true,
+-- }
+local inlay_hints = {
+  includeInlayParameterNameHints = 'all',
+  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  includeInlayFunctionParameterTypeHints = true,
+  includeInlayVariableTypeHints = true,
+  includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+  includeInlayPropertyDeclarationTypeHints = true,
+  includeInlayFunctionLikeReturnTypeHints = true,
+  includeInlayEnumMemberValueHints = true,
+}
+
+---@module "vim.lsp.client"
+---@class vim.lsp.ClientConfig
 return {
+  -- filetypes = { 'javascript', 'typescript', 'vue' },
   init_options = {
     plugins = {
       {
         name = '@vue/typescript-plugin',
         location = vue_typescript_plugin,
         languages = { 'vue' },
+        -- languages = { "javascript", "typescript", "vue" },
       },
     },
   },
   settings = {
     typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = 'all',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
+      inlayHints = inlay_hints,
     },
     javascript = {
-      inlayHints = {
-        includeInlayParameterNameHints = 'all',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
+      inlayHints = inlay_hints,
     },
   },
 }
