f6eb6c3d6bec1c47b96c518f6d5c834cddf53de4
Christoph Zirkelbach <christoph@tigion.de>
2025-08-05 12:00:53 +0200

    refactor(nvim): update color pattern highlighting with mini.highpatterns

--- nvim/lua/tigion/plugins/ui/mini-hipatterns.lua
+++ nvim/lua/tigion/plugins/ui/mini-hipatterns.lua
@@ -14,23 +14,29 @@ return {
     require('mini.hipatterns').setup(opts)
 
     local hipatterns = require('mini.hipatterns')
-
-    local rgb_to_hex = require('tigion.core.util').color.rgb_to_hex
-    local rgba_to_hex = require('tigion.core.util').color.rgba_to_hex
-    local hsl_to_hex = require('tigion.core.util').color.hsl_to_hex
-    local hsla_to_hex = require('tigion.core.util').color.hsla_to_hex
-    local cmyk_to_hex = require('tigion.core.util').color.cmyk_to_hex
+    local util_color = require('tigion.core.util').color
 
     -- Table with my styles and the corresponding group style in `MiniHipatterns.compute_hex_color_group`
-    local hi_group_styles = { fg = 'fg', bg = 'bg', line = 'line', inline = 'fg' }
-    -- The global style for my custom highlights
-    local my_style = 'inline' -- 'fg', 'bg', 'line' or 'inline'
+    local hi_group_styles = { fg = 'fg', bg = 'bg', line = 'line', full = 'bg', inline = 'fg' }
+    -- The global style for my custom color pattern highlights
+    local my_style = 'inline'
+    local my_inline_text = ' ' --  󱓻  󰝤
+
+    -- Returns hex color group for matching hex color.
+    ---@param match string
+    ---@return string
+    local hex_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
+      local r, g, b = match:sub(2, 3), match:sub(4, 5), match:sub(6, 7)
+      local hex = string.format('#%s%s%s', r, g, b)
+      return hipatterns.compute_hex_color_group(hex, style)
+    end
 
     -- Returns hex color group for matching short hex color.
     ---@param match string
     ---@return string
-    local hex_color_short = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local hex_color_short_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
       local hex = string.format('#%s%s%s%s%s%s', r, r, g, g, b, b)
       return hipatterns.compute_hex_color_group(hex, style)
@@ -39,10 +45,10 @@ return {
     -- Returns hex color group for matching rgb() color.
     ---@param match string
     ---@return string
-    local rgb_color = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local rgb_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local r, g, b = match:match('rgb%((%d+), ?(%d+), ?(%d+)%)')
-      local hex = rgb_to_hex(r, g, b)
+      local hex = util_color.rgb_to_hex(r, g, b)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
@@ -50,22 +56,22 @@ return {
     -- The use of the alpha value refers to a black background.
     ---@param match string
     ---@return string|false
-    local rgba_color = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local rgba_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local r, g, b, a = match:match('rgba%((%d+), ?(%d+), ?(%d+), ?(%d*%.?%d*)%)')
       a = tonumber(a)
       if a == nil then return false end
-      local hex = rgba_to_hex(r, g, b, a)
+      local hex = util_color.rgba_to_hex(r, g, b, a)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns hex color group for matching hsl() color.
     ---@param match string
     ---@return string --, integer, integer, integer
-    local hsl_color = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local hsl_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local h, s, l = match:match('hsl%((%d+), ?(%d+)%%, ?(%d+)%%%)')
-      local hex = hsl_to_hex(h, s, l)
+      local hex = util_color.hsl_to_hex(h, s, l)
       return hipatterns.compute_hex_color_group(hex, style) --, red, green, blue
     end
 
@@ -73,22 +79,22 @@ return {
     -- The use of the alpha value refers to a black background.
     ---@param match string
     ---@return string|false
-    local hsla_color = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local hsla_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local h, s, l, a = match:match('hsla%((%d+), ?(%d+)%%, ?(%d+)%%, ?(%d*%.?%d*)%)')
       a = tonumber(a)
       if a == nil then return false end
-      local hex = hsla_to_hex(h, s, l, a)
+      local hex = util_color.hsla_to_hex(h, s, l, a)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
     -- Returns hex color group for matching cmyk() color.
     ---@param match string
     ---@return string
-    local cmyk_color = function(_, match)
-      local style = hi_group_styles[my_style] or 'bg' -- 'fg', 'bg' or 'line'
+    local cmyk_color_group = function(_, match)
+      local style = hi_group_styles[my_style] or 'bg'
       local c, m, y, k = match:match('cmyk%((%d+)%%, ?(%d+)%%, ?(%d+)%%, ?(%d+)%%%)')
-      local hex = cmyk_to_hex(c, m, y, k)
+      local hex = util_color.cmyk_to_hex(c, m, y, k)
       return hipatterns.compute_hex_color_group(hex, style)
     end
 
@@ -97,9 +103,9 @@ return {
     ---@return table
     local extmark_opts_inline = function(_, _, data)
       return {
-        virt_text = { { ' ', data.hl_group } },
+        virt_text = { { my_inline_text, data.hl_group } },
         virt_text_pos = 'inline',
-        -- priority = 200,
+        priority = 200,
         right_gravity = false,
       }
     end
@@ -109,51 +115,57 @@ return {
 
     hipatterns.setup({
       highlighters = {
-        -- `#ff0000`
+        -- `#00ff00`
         -- hex_color = hipatterns.gen_highlighter.hex_color({ style = 'full' }),
-        hex_color = hipatterns.gen_highlighter.hex_color({ style = 'inline', inline_text = ' ' }),
+        -- hex_color = hipatterns.gen_highlighter.hex_color({ style = 'inline', inline_text = ' ' }),
 
-        -- `#f00`
-        -- Checks:
-        -- - true: #0f0 '#0f0'  "#0f0" #0f0 :#0f0 (#0f0)
-        -- - false: #f #f0 #f000 #f0000 #f00tor (like #factor) #facc #f00ö (like #dafür)
-        -- - Allow? #ff0#ff0
+        -- True:  #00ff00 '#00ff00' "#00ff00" :#00ff00 (#00ff00) #ffff00#ffff00
+        -- False: #f #ff #ff00 #ff000 #ff0000tor #ff0000ö
+        hex_color = {
+          pattern = '#%x%x%x%x%x%x%f[%p%s%c%z]',
+          group = hex_color_group,
+          extmark_opts = my_extmark_opts,
+        },
+
+        -- True:  #0f0 '#0f0' "#0f0" :#0f0 (#0f0) #ff0#ff0
+        -- False: #f #f0 #f000 #f0000 #f00tor (like #factor) #facc #f00ö (like #dafür)
         hex_color_short = {
           -- pattern = '#%x%x%x%f[%X]', -- ! `#f00tor`, `#f00ö`
           -- pattern = '()#%x%x%x()%f[^%x%w]', -- ! match `#f00ö`
           pattern = '#%x%x%x%f[%p%s%c%z]',
-          group = hex_color_short,
+          group = hex_color_short_group,
           extmark_opts = my_extmark_opts,
         },
 
-        -- `rgb(255, 0, 0)`
-        rgb_color = { pattern = 'rgb%(%d+, ?%d+, ?%d+%)', group = rgb_color, extmark_opts = my_extmark_opts },
+        -- `rgb(0, 255, 0)`
+        rgb_color = { pattern = 'rgb%(%d+, ?%d+, ?%d+%)', group = rgb_color_group, extmark_opts = my_extmark_opts },
 
-        -- `rgba(255, 0, 0, 0.5)`
+        -- `rgba(0, 255, 0, 0.5)`
         rgba_color = {
           pattern = 'rgba%(%d+, ?%d+, ?%d+, ?%d*%.?%d*%)',
-          group = rgba_color,
+          group = rgba_color_group,
           extmark_opts = my_extmark_opts,
         },
 
-        -- `hsl(0, 100%, 50%)`
+        -- `hsl(100, 100%, 50%)`
         hsl_color = {
           pattern = 'hsl%(%d+, ?%d+%%, ?%d+%%%)',
-          group = hsl_color,
+          group = hsl_color_group,
           extmark_opts = my_extmark_opts,
         },
 
-        -- `hsla(0, 100%, 50%, 0.5)`
+        -- `hsla(100, 100%, 50%, 0.5)`
         hsla_color = {
           pattern = 'hsla%(%d+, ?%d+%%, ?%d+%%, ?%d*%.?%d*%)',
-          group = hsla_color,
+          group = hsla_color_group,
           extmark_opts = my_extmark_opts,
         },
 
-        -- `cmyk(0%, 100%, 100%, 0%)`
+        -- `cmyk(100%, 0%, 100%, 0%)`
+        -- `cmyk(100%, 0%, 100%, 50%)`
         cmyk_color = {
           pattern = 'cmyk%(%d+%%, ?%d+%%, ?%d+%%, ?%d+%%%)',
-          group = cmyk_color,
+          group = cmyk_color_group,
           extmark_opts = my_extmark_opts,
         },
       },
