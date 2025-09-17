3303573658e79c84e9bb22680b3cc393c9d0bb1e
Christoph Zirkelbach <christoph@tigion.de>
2025-09-17 12:45:27 +0200

    feat(nvim): add incremental selection to flash.nvim config

--- nvim/lua/tigion/plugins/navigating.lua
+++ nvim/lua/tigion/plugins/navigating.lua
@@ -63,6 +63,12 @@ return {
       { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
       { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
       { '<C-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
+      {
+        '<Return>',
+        mode = { 'n', 'x', 'o' },
+        function() require('flash').treesitter({ actions = { ['<Return>'] = 'next', ['<BS>'] = 'prev' } }) end,
+        desc = 'Treesitter incremental selection',
+      },
     },
   },
 
