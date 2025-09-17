409ab5cbe6216843c74853b1a8597b32395f1ea0
Christoph Zirkelbach <christoph@tigion.de>
2025-09-17 16:56:15 +0200

    feat(nvim): update html lsp config

--- nvim/after/lsp/html.lua
+++ nvim/after/lsp/html.lua
@@ -4,7 +4,11 @@ return {
       format = {
         templating = true,
         wrapLineLength = 0,
+        wrapAttributes = 'preserve-aligned',
+        -- wrapAttributes = 'auto',
       },
     },
   },
+  -- filetypes = { 'html', 'templ' }, -- default
+  filetypes = { 'html', 'templ', 'htmldjango' },
 }
