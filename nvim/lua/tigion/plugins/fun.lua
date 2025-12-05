f5ba7022981739a01c8ebdbb0432b7541c92c148
Christoph Zirkelbach <christoph@tigion.de>
2025-12-05 15:34:34 +0100

    feat(nvim): add games.nvim config

---
+++ nvim/lua/tigion/plugins/fun.lua
@@ -0,0 +1,16 @@
+return {
+  'tigion/games.nvim',
+  enable = true,
+  dev = true,
+  event = 'VeryLazy',
+  -- keys = {},
+  opts = {
+    window = {
+      -- width = 1,
+      -- height = 1,
+      -- max = { width = 0, height = 0 },
+      border = 'rounded',
+      -- ignore_34_aspect_ratio = true,
+    },
+  },
+}
