529e5990e977fae183cdaf1efb6dd0d53e2a8fad
Christoph Zirkelbach <christoph@tigion.de>
2026-03-15 14:19:42 +0100

    fix(nvim): correct build instructions for markdown-preview.nvim

--- nvim/lua/tigion/plugins/filetype/markdown.lua
+++ nvim/lua/tigion/plugins/filetype/markdown.lua
@@ -66,6 +66,10 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
-    build = function() vim.fn['mkdp#util#install']() end,
+    -- build = function() vim.fn['mkdp#util#install']() end,
+    build = function()
+      require('lazy').load({ plugins = { 'markdown-preview.nvim' } })
+      vim.fn['mkdp#util#install']()
+    end,
   },
 }
