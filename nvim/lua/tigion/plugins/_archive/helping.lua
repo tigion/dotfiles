890aeb404a0acbf29e5f83a42b6cbf34f75b7eeb
Christoph Zirkelbach <christoph@tigion.de>
2026-04-11 20:12:30 +0200

    feat(nvim): remove (archive) some helper plugins

---
+++ nvim/lua/tigion/plugins/_archive/helping.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    -- This plugin assists with motions to navigate your current buffer in Neovim.
+    -- Link: https://github.com/tris203/precognition.nvim
+
+    'tris203/precognition.nvim',
+    enabled = false,
+    event = 'VeryLazy',
+    keys = {
+      { '<Leader>tp', '<Cmd>Precognition toggle<CR>', desc = 'Toggle Precognition' },
+    },
+    opts = {
+      startVisible = false,
+    },
+  },
+
+  {
+    -- This plugin establishes a good command workflow and
+    -- quits bad habits in Neovim.
+    -- Link: https://github.com/m4xshen/hardtime.nvim
+
+    'm4xshen/hardtime.nvim',
+    enabled = false,
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    event = 'VeryLazy',
+    keys = {
+      { '<Leader>tha', '<Cmd>Hardtime toggle<CR>', desc = 'Toggle Hardtime' },
+    },
+    opts = {
+      disable_mouse = false,
+      restriction_mode = 'hint',
+      disabled_keys = {
+        ['<Up>'] = { '' },
+        ['<Down>'] = { '' },
+        ['<Left>'] = { '' },
+        ['<Right>'] = { '' },
+      },
+    },
+  },
+}
